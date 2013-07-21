5a58891bfd847f416e27879cfccd79fb213ae05c
Audrey Roy <audreyr@gmail.com>
2013-07-22 00:28:11 +0200

    Don't accept output_dir arg anymore.

--- cookiecutter/main.py
+++ cookiecutter/main.py
@@ -16,17 +16,11 @@ def main():
         'input_dir',
         help='Cookiecutter project template dir, e.g. {{project.repo_name}}/'
     )
-    parser.add_argument(
-        'output_dir',
-        help='Name of desired output dir, e.g. alotofeffort/ or'
-            '{{project.repo_name}}/. (This can be templated.)'
-    )
     args = parser.parse_args()
 
     context = generate_context()
     generate_files(
         input_dir=args.input_dir,
-        output_dir=args.output_dir,
         context=context
     )
 
