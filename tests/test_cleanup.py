8c3a81a73aeac7744a8b501d1016abd6a6ee67fd
Audrey Roy <audreyr@gmail.com>
2013-08-10 12:23:44 +0200

    Test for exception during cleanup.

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -12,7 +12,7 @@ import os
 import shutil
 import unittest
 
-from cookiecutter import cleanup
+from cookiecutter import cleanup, exceptions
 
 
 class TestCleanup(unittest.TestCase):
@@ -26,6 +26,14 @@ class TestCleanup(unittest.TestCase):
         self.assertTrue(os.path.isdir('tests/fake-project'))
         self.assertTrue(os.path.isfile('tests/fake-project/README.rst'))
         self.assertFalse(os.path.exists('tests/fake-repo'))
+
+    def test_remove_repo_bad(self):
+        self.assertRaises(
+            exceptions.MissingProjectDir,
+            cleanup.remove_repo,
+            repo_dir='tests/fake-repo-bad',
+            generated_project='fake-project'
+        )
 
     def tearDown(self):
         if not os.path.exists('tests/fake-repo'):
