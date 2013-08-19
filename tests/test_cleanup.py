5e23deb7106df1b40b65a3afff10f09655553f0f
Audrey Roy <audreyr@gmail.com>
2013-08-19 23:07:07 +0200

    Better test behavior on systems that don't support unicode filenames.

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -20,13 +20,24 @@ class TestCleanup(unittest.TestCase):
 
     def test_remove_repo(self):
         success = cleanup.remove_repo(
-            repo_dir='tests/fake-repö',
-            generated_project='fake-pröject'
+            repo_dir='tests/fake-repo',
+            generated_project='fake-project'
         )
         self.assertTrue(success)
-        self.assertTrue(os.path.isdir('tests/fake-pröject'))
-        self.assertTrue(os.path.isfile('tests/fake-pröject/README.rst'))
-        self.assertFalse(os.path.exists('tests/fake-repö'))
+        self.assertTrue(os.path.isdir('tests/fake-project'))
+        self.assertTrue(os.path.isfile('tests/fake-project/README.rst'))
+        self.assertFalse(os.path.exists('tests/fake-repo'))
+
+    def test_remove_repo_unicode(self):
+        if os.path.supports_unicode_filenames:
+            success = cleanup.remove_repo(
+                repo_dir='tests/fake-repööö',
+                generated_project='fake-pröööject'
+            )
+            self.assertTrue(success)
+            self.assertTrue(os.path.isdir('tests/fake-pröööject'))
+            self.assertTrue(os.path.isfile('tests/fake-pröööject/README.rst'))
+            self.assertFalse(os.path.exists('tests/fake-repööö'))
 
     def test_remove_repo_bad(self):
         self.assertRaises(
@@ -37,9 +48,15 @@ class TestCleanup(unittest.TestCase):
         )
 
     def tearDown(self):
-        if not os.path.exists('tests/fake-repö'):
-            os.mkdir('tests/fake-repö')
-            shutil.move('tests/fake-pröject', 'tests/fake-repö/fake-pröject')
+        if not os.path.exists('tests/fake-repo'):
+            os.mkdir('tests/fake-repo')
+            shutil.move('tests/fake-project', 'tests/fake-repo/fake-project')
+
+        if os.path.supports_unicode_filenames:
+            if not os.path.exists('tests/fake-repööö'):
+                os.mkdir('tests/fake-repööö')
+                shutil.move('tests/fake-pröööject', 'tests/fake-repööö/fake-pröööject')
+
 
 if __name__ == '__main__':
     unittest.main()
