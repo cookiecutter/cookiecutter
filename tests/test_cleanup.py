e847b0d0d5527fea0e3295cd767528ffa5e25705
Audrey Roy <audreyr@gmail.com>
2013-08-19 22:30:14 +0200

    Unicöde directory name tests in test_cleanup.

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -8,6 +8,7 @@ test_cleanup
 Tests for `cookiecutter.cleanup` module.
 """
 
+from __future__ import unicode_literals
 import os
 import shutil
 import unittest
@@ -19,13 +20,13 @@ class TestCleanup(unittest.TestCase):
 
     def test_remove_repo(self):
         success = cleanup.remove_repo(
-            repo_dir='tests/fake-repo',
+            repo_dir='tests/fake-repö',
             generated_project='fake-project'
         )
         self.assertTrue(success)
         self.assertTrue(os.path.isdir('tests/fake-project'))
         self.assertTrue(os.path.isfile('tests/fake-project/README.rst'))
-        self.assertFalse(os.path.exists('tests/fake-repo'))
+        self.assertFalse(os.path.exists('tests/fake-repö'))
 
     def test_remove_repo_bad(self):
         self.assertRaises(
@@ -36,9 +37,9 @@ class TestCleanup(unittest.TestCase):
         )
 
     def tearDown(self):
-        if not os.path.exists('tests/fake-repo'):
-            os.mkdir('tests/fake-repo')
-            shutil.move('tests/fake-project', 'tests/fake-repo/fake-project')
+        if not os.path.exists('tests/fake-repö'):
+            os.mkdir('tests/fake-repö')
+            shutil.move('tests/fake-project', 'tests/fake-repö/fake-project')
 
 if __name__ == '__main__':
     unittest.main()
