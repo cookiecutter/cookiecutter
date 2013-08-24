221a58f21c2c2224134b1565b018fb0d60bb38e4
raphigaziano <r.gaziano@gmail.com>
2013-08-24 13:01:05 +0200

    removed common path in hook finder test

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -17,17 +17,21 @@ from cookiecutter import hooks
 
 class TestFindHooks(unittest.TestCase):
 
-    repo_path = 'tests/fake-repo-hooks'
-
     def test_find_hooks(self):
         '''Getting the list of all defined hooks'''
+        repo_path = 'tests/fake-repo-hooks'
         self.assertEqual({
             'pre_gen_project': os.path.abspath(
-                os.path.join(self.repo_path, 'hooks', 'pre_gen_project.py')),
+                os.path.join(repo_path, 'hooks', 'pre_gen_project.py')),
             'post_gen_project': os.path.abspath(
-                os.path.join(self.repo_path, 'hooks', 'post_gen_project.sh')),
-        }, hooks.find_hooks(self.repo_path))
+                os.path.join(repo_path, 'hooks', 'post_gen_project.sh')),
+        }, hooks.find_hooks(repo_path))
 
     def test_no_hooks(self):
         '''find_hooks should return an empty dict if no hooks folder could be found. '''
         self.assertEqual({}, hooks.find_hooks('tests/fake-repo'))
+
+
+class TestHooks(unittest.TestCase):
+
+    repo_path = 'tests/fake-repo-hooks'
