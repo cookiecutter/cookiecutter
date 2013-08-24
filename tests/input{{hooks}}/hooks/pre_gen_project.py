19d56c8411ecf2260bf31980871db5c04e388dd5
raphigaziano <r.gaziano@gmail.com>
2013-08-24 18:30:11 +0200

    dummy scripts can now run under py3

--- tests/input{{hooks}}/hooks/pre_gen_project.py
+++ tests/input{{hooks}}/hooks/pre_gen_project.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 
-print 'pre generation hook'
+print('pre generation hook')
 f = open('bar.txt', 'w')
 f.close()
 
