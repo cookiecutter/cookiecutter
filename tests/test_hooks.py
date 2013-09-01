be48478b98b433ba0c0e5dc69a981b100fcf4a22
Audrey Roy <audreyr@gmail.com>
2013-09-01 15:44:08 +0200

    Fix test_hooks again.

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -38,32 +38,34 @@ class TestExternalHooks(unittest.TestCase):
     hooks_path = os.path.join(repo_path, 'hooks')
 
     def tearDown(self):
-        if os.path.exists('foo.txt'):
-            os.remove('foo.txt')
-        if os.path.exists('tests/foo.txt'):
-            os.remove('tests/foo.txt')
-        if os.path.exists('tests/bar.txt'):
-            os.remove('tests/bar.txt')
+        if os.path.exists('python_pre.txt'):
+            os.remove('python_pre.txt')
+        if os.path.exists('shell_post.txt'):
+            os.remove('shell_post.txt')
+        if os.path.exists('tests/python_pre.txt'):
+            os.remove('tests/python_pre.txt')
+        if os.path.exists('tests/shell_post.txt'):
+            os.remove('tests/shell_post.txt')
 
     def test_run_hook(self):
         '''execute a hook script, independently of project generation'''
         hooks._run_hook(os.path.join(self.hooks_path, 'post_gen_project.sh'))
-        self.assertTrue(os.path.isfile('foo.txt'))
+        self.assertTrue(os.path.isfile('shell_post.txt'))
 
     def test_run_hook_cwd(self):
         '''Change directory before running hook'''
         hooks._run_hook(os.path.join(self.hooks_path, 'post_gen_project.sh'), 
                         'tests')
-        self.assertTrue(os.path.isfile('tests/foo.txt'))
+        self.assertTrue(os.path.isfile('tests/shell_post.txt'))
         self.assertFalse('tests' in os.getcwd())
         
     def test_public_run_hook(self):
         '''Execute hook from specified template in specified output directory'''
         hooks.run_hook('pre_gen_project', self.repo_path, 'tests')
-        self.assertTrue(os.path.isfile('tests/bar.txt'))
+        self.assertTrue(os.path.isfile('tests/python_pre.txt'))
 
         hooks.run_hook('post_gen_project', self.repo_path, 'tests')
-        self.assertTrue(os.path.isfile('tests/foo.txt'))
+        self.assertTrue(os.path.isfile('tests/shell_post.txt'))
 
 
 if __name__ == '__main__':
