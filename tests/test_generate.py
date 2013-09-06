4cf6b502d14422fa72170ed0d03e8d1493ec4a3c
Audrey Roy <audreyr@gmail.com>
2013-09-06 17:03:05 +0200

    Fix output folder test.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -164,30 +164,30 @@ class TestOutputFolder(unittest.TestCase):
 
     def test_output_folder(self):
         context = generate.generate_context(
-            config_file='tests/json2/stuff.json'
+            config_file='tests/test-output-folder/cookiecutter.json'
         )
         logging.debug('Context is {0}'.format(context))
         generate.generate_files(
             context=context,
-            template_dir='tests/input{{stuff.color}}'
+            repo_dir='tests/test-output-folder'
         )
 
         something = """Hi!
 My name is Audrey Greenfeld.
 It is 2014."""
-        something2 = open('tests/inputgreen/something.txt').read()
+        something2 = open('output_folder/something.txt').read()
         self.assertEqual(something, something2)
 
         in_folder = "The color is green and the letter is D."
-        in_folder2 = open('tests/inputgreen/folder/in_folder.txt').read()
+        in_folder2 = open('output_folder/folder/in_folder.txt').read()
         self.assertEqual(in_folder, in_folder2)
 
-        self.assertTrue(os.path.isdir('tests/inputgreen/im_a.dir'))
-        self.assertTrue(os.path.isfile('tests/inputgreen/im_a.dir/im_a.file.py'))
+        self.assertTrue(os.path.isdir('output_folder/im_a.dir'))
+        self.assertTrue(os.path.isfile('output_folder/im_a.dir/im_a.file.py'))
 
     def tearDown(self):
-        if os.path.exists('tests/inputgreen'):
-            shutil.rmtree('tests/inputgreen')
+        if os.path.exists('output_folder'):
+            shutil.rmtree('output_folder')
 
 
 class TestHooks(unittest.TestCase):
