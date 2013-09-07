7ff0e4fb9926d49192b49e81e2213ee470852bd0
Audrey Roy <audreyr@gmail.com>
2013-09-07 17:10:57 +0200

    Move CookiecutterTestCase into tests/__init__.py, and remove its dependency on config.

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,9 +12,9 @@ import logging
 import os
 import shutil
 import sys
-import unittest
 
 from cookiecutter import config, main
+from tests import CookiecutterTestCase
 
 if sys.version_info[:2] < (2, 7):
     import unittest2 as unittest
@@ -89,43 +89,6 @@ class TestArgParsing(unittest.TestCase):
         args = main.parse_cookiecutter_args(['project/', '--checkout', 'develop'])
         self.assertEqual(args.input_dir, 'project/')
         self.assertEqual(args.checkout, 'develop')
-
-
-class CookiecutterTestCase(unittest.TestCase):
-    def setUp(self):
-        # If ~/.cookiecutterrc is pre-existing, move it to a temp location
-        self.user_config_path = os.path.expanduser('~/.cookiecutterrc')
-        self.user_config_path_backup = os.path.expanduser(
-            '~/.cookiecutterrc.backup'
-        )
-        if os.path.exists(self.user_config_path):
-            shutil.copy(self.user_config_path, self.user_config_path_backup)
-            os.remove(self.user_config_path)
-
-        # If the default cookiecutters_dir is pre-existing, move it to a
-        # temp location
-        self.cookiecutters_dir = config.DEFAULT_CONFIG['cookiecutters_dir']
-        self.cookiecutters_dir_backup = os.path.expanduser('~/.cookiecutters.backup')
-        if os.path.isdir(self.cookiecutters_dir):
-            shutil.copytree(self.cookiecutters_dir, self.cookiecutters_dir_backup)
-        if os.path.isdir(self.cookiecutters_dir_backup):
-            shutil.rmtree(self.cookiecutters_dir)
-
-    def tearDown(self):
-        # Delete the created ~/.cookiecutters dir as long as a backup exists
-        if os.path.isdir(self.cookiecutters_dir) and os.path.isdir(self.cookiecutters_dir_backup):
-            shutil.rmtree(self.cookiecutters_dir)
-    
-        # If it existed, restore ~/.cookiecutterrc
-        if os.path.exists(self.user_config_path_backup):
-            shutil.copy(self.user_config_path_backup, self.user_config_path)
-            os.remove(self.user_config_path_backup)
-        
-        # Restore the user's default cookiecutters_dir contents
-        if os.path.isdir(self.cookiecutters_dir_backup):
-            shutil.copytree(self.cookiecutters_dir_backup, self.cookiecutters_dir)
-        if os.path.isdir(self.cookiecutters_dir):
-            shutil.rmtree(self.cookiecutters_dir_backup)
 
 
 class TestCookiecutterRepoArg(CookiecutterTestCase):
