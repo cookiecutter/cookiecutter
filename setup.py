4a4c4082239171ff5bf77068356b9750b5d83200
Éric Araujo <merwok@netwok.org>
2014-05-02 03:54:28 -0400

    Avoid unwanted extra spaces in string literal

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ if sys.version < '3':
 setup(
     name='cookiecutter',
     version='0.7.1',
-    description='A command-line utility that creates projects from project \
-        templates, e.g. creating a Python package project from a Python \
-        package project template.',
+    description=('A command-line utility that creates projects from project '
+                 'templates, e.g. creating a Python package project from a Python '
+                 'package project template.'),
     long_description=readme + '\n\n' + history,
     author='Audrey Roy',
     author_email='audreyr@gmail.com',
