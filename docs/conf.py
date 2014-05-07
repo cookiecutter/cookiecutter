750b96105f557b67042de4cf586b8da78c1e6f2f
sax <s.apostolico@gmail.com>
2014-05-07 20:43:31 +0200

    add documentation about command line arguments
    text is auto generated from the arguments help text
    
    ref. #166

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,9 @@ import cookiecutter
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx',
+              'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath',
+              'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'docs.ccext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
