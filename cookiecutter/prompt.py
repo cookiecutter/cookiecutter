9396177179fcb20478aee4868b2228c217221616
Lukasz Boldys <mail@utek.pl>
2014-05-21 07:41:18 +0200

    using stdin encoding

--- cookiecutter/prompt.py
+++ cookiecutter/prompt.py
@@ -32,7 +32,8 @@ def prompt_for_config(context):
         if PY3:
             new_val = input(prompt.encode('utf-8'))
         else:
-            new_val = input(prompt.encode('utf-8')).decode('utf-8')
+            stdin_enc = sys.stdin.encoding
+            new_val = input(prompt.encode('utf-8')).decode(stdin_enc)
 
         new_val = new_val.strip()
 
