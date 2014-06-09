601649e4a3c31d1984684dedb586b18a092ed58e
Lucian Ursu <lucian.ursu@gmail.com>
2014-06-09 16:30:39 +0300

    Did more renaming and added more tests.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -188,31 +188,46 @@ class TestGenerateFiles(CookiecutterCleanSystemTestCase):
             os.stat('inputpermissions/script.sh').st_mode & 0o777
         )
 
-    def test_generate_ignore_file_extensions(self):
+    def test_generate_copy_without_render_extensions(self):
         generate.generate_files(
             context={
                 'cookiecutter': {
-                    "repo_name": "test_ignore",
-                    "do_not_ignore": "Do not ignore!",
-                    "_ignore_files": ["*.txt"]}
+                    "repo_name": "test_copy_without_render",
+                    "render_test": "I have been rendered!",
+                    "_copy_without_render": [
+                        "*not-rendered",
+                        "rendered/not_rendered.yml",
+                        "*.txt",
+                    ]}
             },
-            repo_dir='tests/test-generate-ignore'
+            repo_dir='tests/test-generate-copy-without-render'
         )
 
-        with open("test_ignore/README.txt") as f:
-            self.assertIn("{{cookiecutter.do_not_ignore}}", f.read())
+        self.assertIn("{{cookiecutter.repo_name}}-not-rendered",
+                      os.listdir("test_copy_without_render"))
+        self.assertIn("test_copy_without_render-rendered",
+                      os.listdir("test_copy_without_render"))
 
-        with open("test_ignore/README.rst") as f:
-            self.assertIn("Do not ignore!", f.read())
+        with open("test_copy_without_render/README.txt") as f:
+            self.assertIn("{{cookiecutter.render_test}}", f.read())
 
-        with open("test_ignore/test_ignore-sub/README.txt") as f:
-            self.assertIn("{{cookiecutter.do_not_ignore}}", f.read())
+        with open("test_copy_without_render/README.rst") as f:
+            self.assertIn("I have been rendered!", f.read())
 
-        with open("test_ignore/test_ignore-sub/README.rst") as f:
-            self.assertIn("Do not ignore!", f.read())
+        with open("test_copy_without_render/test_copy_without_render-rendered/README.txt") as f:
+            self.assertIn("{{cookiecutter.render_test}}", f.read())
 
-        if os.path.exists('test_ignore'):
-            shutil.rmtree('test_ignore')
+        with open("test_copy_without_render/test_copy_without_render-rendered/README.rst") as f:
+            self.assertIn("I have been rendered", f.read())
+
+        with open("test_copy_without_render/{{cookiecutter.repo_name}}-not-rendered/README.rst") as f:
+            self.assertIn("{{cookiecutter.render_test}}", f.read())
+
+        with open("test_copy_without_render/rendered/not_rendered.yml") as f:
+            self.assertIn("{{cookiecutter.render_test}}", f.read())
+
+        if os.path.exists('test_copy_without_render'):
+            shutil.rmtree('test_copy_without_render')
 
 
 class TestGenerateContext(CookiecutterCleanSystemTestCase):
