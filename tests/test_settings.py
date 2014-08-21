802d958e729ae21155375e43292d36e8555e8790
Paul Moore <p.f.moore@gmail.com>
2014-08-21 17:32:43 +0100

    Refactor settings so that clients don't need to know the structure of the data

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -8,10 +8,14 @@ test_settings
 Tests for `cookiecutter.settings` module.
 """
 
+import os
 import sys
+import tempfile
+from contextlib import contextmanager
 
 from cookiecutter import settings
 from cookiecutter import exceptions
+
 
 if sys.version_info[:2] < (2, 7):
     import unittest2 as unittest
@@ -19,33 +23,45 @@ else:
     import unittest
 
 
+@contextmanager
+def temp(content=None, suffix='.yaml'):
+    name = None
+    try:
+        with tempfile.NamedTemporaryFile(suffix=suffix, delete=False) as f:
+            if content:
+                f.write(content.encode('utf-8'))
+            name = f.name
+        yield name[:-len(suffix)]
+    finally:
+        if name and os.path.exists(name):
+            os.unlink(name)
+
+
 class TestGetConfig(unittest.TestCase):
 
     def test_get_settings(self):
         """ Opening and reading settings file """
-        stg = settings.get_settings('tests/test-settings/valid')
+        template_settings = settings.get_settings(name='tests/test-settings/valid')
         expected_context = {
-            'full_name': 'Firstname Lastname',
-            'email': 'firstname.lastname@gmail.com',
-            'github_username': 'example'
+            'valid': {
+                'full_name': 'Firstname Lastname',
+                'email': 'firstname.lastname@gmail.com',
+                'github_username': 'example'
+            }
         }
-        self.assertTrue('config' in stg)
-        self.assertTrue('context' in stg)
-        self.assertEqual(stg['config'].get('version'), 2)
-        self.assertEqual(stg['context'].get('valid'), expected_context)
+        self.assertEqual(settings.get_context_from_settings(template_settings), expected_context)
 
     def test_get_settings_json(self):
         """ Opening and reading old-style JSON settings file """
-        stg = settings.get_settings('tests/test-settings/valid-json')
+        template_settings = settings.get_settings('tests/test-settings/valid-json')
         expected_context = {
-            'full_name': 'Firstname Lastname',
-            'email': 'firstname.lastname@gmail.com',
-            'github_username': 'example'
+            'valid-json': {
+                'full_name': 'Firstname Lastname',
+                'email': 'firstname.lastname@gmail.com',
+                'github_username': 'example'
+            }
         }
-        self.assertTrue('config' in stg)
-        self.assertTrue('context' in stg)
-        self.assertEqual(stg['config'].get('version'), 1)
-        self.assertEqual(stg['context'].get('valid-json'), expected_context)
+        self.assertEqual(settings.get_context_from_settings(template_settings), expected_context)
 
     def test_get_settings_does_not_exist(self):
         """
@@ -66,6 +82,30 @@ class TestGetConfig(unittest.TestCase):
             settings.get_settings,
             'tests/test-settings/both'
         )
+
+    def test_version_of_empty_yaml_is_2(self):
+        """
+        Check that an empty YAML file is reported as version 2
+        """
+        with temp('') as name:
+            template_settings = settings.get_settings(name)
+            self.assertEqual(settings.get_version_from_settings(template_settings), 2)
+
+    def test_version_is_read_from_yaml(self):
+        """
+        Check that an explicit version is read from a YAML file
+        """
+        with temp('config:\n    version: 3') as name:
+            template_settings = settings.get_settings(name)
+            self.assertEqual(settings.get_version_from_settings(template_settings), 3)
+
+    def test_json_version_is_1(self):
+        """
+        Check that a JSON format file is reported as version 1
+        """
+        with temp('{}', suffix='.json') as name:
+            template_settings = settings.get_settings(name)
+            self.assertEqual(settings.get_version_from_settings(template_settings), 1)
 
 
 if __name__ == '__main__':
