055d2bd174d8d0ae61a4b94514ab6637540b2ed4
Audrey Roy <audreyr@gmail.com>
2014-09-01 20:16:54 -0700

    Break extra_context test into 2 tests, to cover both combinations with default_context.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -204,12 +204,22 @@ class TestGenerateContext(CookiecutterCleanSystemTestCase):
         self.assertEqual(context, {"test": {"1": 3, "some_key": "some_val"}})
 
     def test_generate_context_with_extra(self):
+        """ Call `generate_context()` with extra_context. """
         context = generate.generate_context(
             context_file='tests/test-generate-context/test.json',
-            default_context={'1': 3},
             extra_context={'1': 4},
         )
         self.assertEqual(context, {'test': {'1': 4, 'some_key': 'some_val'}})
+
+    def test_generate_context_with_default_and_extra(self):
+        """ Call `generate_context()` with `default_context` and 
+            `extra_context`. """
+        context = generate.generate_context(
+            context_file='tests/test-generate-context/test.json',
+            default_context={'1': 3},
+            extra_context={'1': 5},
+        )
+        self.assertEqual(context, {'test': {'1': 5, 'some_key': 'some_val'}})
 
 
 class TestOutputFolder(CookiecutterCleanSystemTestCase):
