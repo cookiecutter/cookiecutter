b70c47f0da9c8a2d8fda8c32e39f273b8b73d34e
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-10-22 23:40:33 -0400

    emulating JSON OrderedDict usage in test

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -15,7 +15,6 @@ from cookiecutter.compat import patch, unittest
 from cookiecutter import prompt
 
 if 'windows' in platform.platform().lower():
-
     old_stdin = sys.stdin
 
     class X(object):
@@ -56,8 +55,10 @@ class TestPrompt(unittest.TestCase):
 
     @patch('cookiecutter.prompt.read_response', lambda x=u'': u'\n')
     def test_unicode_prompt_for_templated_config(self):
-        context = {"cookiecutter": {"project_name": u"A New Project",
-            "pkg_name": "{{ project_name|replace(' ', '')|lower }}"}}
+        context = {"cookiecutter": OrderedDict([
+            ("project_name", u"A New Project"),
+            ("pkg_name", u"{{ project_name|lower|replace(' ', '') }}")
+        ])}
 
         cookiecutter_dict = prompt.prompt_for_config(context)
         self.assertEqual(cookiecutter_dict, {"project_name": u"A New Project",
