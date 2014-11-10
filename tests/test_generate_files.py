329e74f280537aab41d5b810f8650bfd8d6d81f5
Raphael Pierzina <raphael@hackebrot.de>
2014-11-10 22:27:44 +0100

    Add teardown specific to the former TestCase class

--- tests/test_generate_files.py
+++ tests/test_generate_files.py
@@ -9,12 +9,32 @@ Test formerly known from a unittest residing in test_generate.py named
 TestGenerateFiles.test_generate_files_nontemplated_exception
 """
 
+from __future__ import unicode_literals
+import os
 import pytest
+
 from cookiecutter import generate
 from cookiecutter import exceptions
+from cookiecutter import utils
 
 
-@pytest.mark.usefixtures("clean_system")
+@pytest.fixture(scope="function")
+def clean_system_remove_additional_folders(request, clean_system):
+    def remove_additional_folders():
+        if os.path.exists('inputpizzä'):
+            utils.rmtree('inputpizzä')
+        if os.path.exists('inputgreen'):
+            utils.rmtree('inputgreen')
+        if os.path.exists('inputbinary_files'):
+            utils.rmtree('inputbinary_files')
+        if os.path.exists('tests/custom_output_dir'):
+            utils.rmtree('tests/custom_output_dir')
+        if os.path.exists('inputpermissions'):
+            utils.rmtree('inputpermissions')
+    request.addfinalizer(remove_additional_folders)
+
+
+@pytest.mark.usefixtures("clean_system_remove_additional_folders")
 def test_generate_files_nontemplated_exception():
     with pytest.raises(exceptions.NonTemplatedInputDirException):
         generate.generate_files(
