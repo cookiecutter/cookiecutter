55bf12c80fe14b317fdc27d74c27642a144d4530
Raphael Pierzina <raphael@hackebrot.de>
2014-11-10 21:23:58 +0100

    Create new test module to convert the first test

---
+++ tests/test_generate_files.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_generate_files
+-------------------
+
+Test formerly known from a unittest residing in test_generate.py named
+TestGenerateFiles.test_generate_files_nontemplated_exception
+"""
+
+
+def test_generate_files_nontemplated_exception():
+    pass
