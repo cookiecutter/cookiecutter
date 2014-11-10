ae535222049b30f18ed4e6f423853ad9ccb74965
Raphael Pierzina <raphael@hackebrot.de>
2014-11-10 21:08:00 +0100

    Create initial conftest.py for global fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Contains pytest fixtures which are globally available throughout the suite.
+"""
+
+import pytest
+
+
+@pytest.fixture
+def foobar():
+    pass
+
