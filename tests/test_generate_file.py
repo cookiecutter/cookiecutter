ee8adcaa217cefa54d38e8c24177646c922e162f
Raphael Pierzina <raphael@hackebrot.de>
2014-11-15 11:17:53 +0100

    Convert template_syntax_error test and fix exception handling

--- tests/test_generate_file.py
+++ tests/test_generate_file.py
@@ -7,6 +7,7 @@ test_generate_file
 
 Tests formerly known from a unittest residing in test_generate.py named
 TestGenerateFile.test_generate_file
+TestGenerateFile.test_generate_file_verbose_template_syntax_error
 """
 
 from __future__ import unicode_literals
@@ -15,6 +16,7 @@ import pytest
 
 from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
+from jinja2.exceptions import TemplateSyntaxError
 
 from cookiecutter import generate
 from cookiecutter import utils
@@ -46,3 +48,28 @@ def test_generate_file():
     with open('tests/files/cheese.txt', 'rt') as f:
         generated_text = f.read()
         assert generated_text == 'Testing cheese'
+
+
+@pytest.mark.usefixtures('remove_cheese_file')
+def test_generate_file_verbose_template_syntax_error():
+    env = Environment()
+    env.loader = FileSystemLoader('.')
+    try:
+        generate.generate_file(
+            project_dir=".",
+            infile='tests/files/syntax_error.txt',
+            context={'syntax_error': 'syntax_error'},
+            env=env
+        )
+    except TemplateSyntaxError as exception:
+        expected = (
+            'Missing end of comment tag\n'
+            '  File "./tests/files/syntax_error.txt", line 1\n'
+            '    I eat {{ syntax_error }} {# this comment is not closed}'
+        )
+        expected = expected.replace("/", os.sep)
+        assert str(exception) == expected
+    except Exception as exception:
+        pytest.fail('Unexpected exception thrown: {0}'.format(exception))
+    else:
+        pytest.fail('TemplateSyntaxError not thrown')
