b242607648be06fac58fc77eeded7238b8f2c1fe
Michael Joseph <michaeljoseph+github@gmail.com>
2014-11-16 14:43:31 +0200

    Warn that we're dropping py26

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -7,5 +7,15 @@ cookiecutter
 
 Main package for Cookiecutter.
 """
+import warnings
+
+from .compat import OLD_PY2
+
 
 __version__ = '0.8.0'
+
+if OLD_PY2:
+    warnings.warn(
+        'Python 2.6 support will be removed in 1.0.0',
+        PendingDeprecationWarning
+    )
