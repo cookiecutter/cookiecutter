6b9c7c3cc40fc91654741cb6f49bcdd1090564e5
Raphael Pierzina <raphael@hackebrot.de>
2014-11-17 07:39:05 +0100

    Fix grammar in doc string

--- tests/test_cookiecutter_local_no_input.py
+++ tests/test_cookiecutter_local_no_input.py
@@ -21,7 +21,7 @@ from cookiecutter import main, utils
 @pytest.fixture(scope='function')
 def remove_additional_dirs(request):
     """
-    Remove special directories which are creating during the tests.
+    Remove special directories which are created during the tests.
     """
     def fin_remove_additional_dirs():
         if os.path.isdir('fake-project'):
