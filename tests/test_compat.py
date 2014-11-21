ce8dcba62df31e19b8f571bacfb57ac2d4681918
Raphael Pierzina <raphael@hackebrot.de>
2014-11-21 23:43:47 +0100

    Convert TestWhich to py.test

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -11,17 +11,9 @@ Tests for `cookiecutter.compat` module.
 from cookiecutter.compat import unittest, which
 
 
-class TestWhich(unittest.TestCase):
+def test_existing_command():
+    assert which('cookiecutter')
 
-    def test_existing_command(self):
-        self.assertTrue(
-            which('cookiecutter')
-        )
 
-    def test_non_existing_command(self):
-        self.assertFalse(
-            which('stringthatisntashellcommand')
-        )
-
-if __name__ == '__main__':
-    unittest.main()
+def test_non_existing_command():
+    assert not which('stringthatisntashellcommand')
