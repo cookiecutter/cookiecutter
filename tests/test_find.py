c375b3dddd292167c0df298165153922e82ffd26
Raphael Pierzina <raphael@hackebrot.de>
2014-11-22 00:58:36 +0100

    Implement a parametrized fixture to cover both tests

--- tests/test_find.py
+++ tests/test_find.py
@@ -9,14 +9,19 @@ Tests for `cookiecutter.find` module.
 """
 
 import os
+import pytest
 import shutil
 import unittest
 
 from cookiecutter import find
 
 
-def test_find_template():
-    repo_dir = os.path.join('tests', 'fake-repo-pre')
+@pytest.fixture(params=['fake-repo-pre', 'fake-repo-pre2'])
+def repo_dir(request):
+    return os.path.join('tests', request.param)
+
+
+def test_find_template(repo_dir):
     template = find.find_template(repo_dir=repo_dir)
 
     test_dir = os.path.join(repo_dir, '{{cookiecutter.repo_name}}')
