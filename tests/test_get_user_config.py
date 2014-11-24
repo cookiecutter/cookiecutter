98b66fd28d0651022a55fb3d32c69a533e395760
Raphael Pierzina <raphael@hackebrot.de>
2014-11-24 20:08:19 +0100

    Remove self references from setup/teardown

--- tests/test_get_user_config.py
+++ tests/test_get_user_config.py
@@ -8,6 +8,8 @@ test_get_user_config
 Tests formerly known from a unittest residing in test_config.py named
 """
 
+import os
+import shutil
 import pytest
 
 
@@ -17,20 +19,20 @@ def back_up_rc(request):
     Back up an existing cookiecutter rc and restore it after the test.
     If ~/.cookiecutterrc is pre-existing, move it to a temp location
     """
-    self.user_config_path = os.path.expanduser('~/.cookiecutterrc')
-    self.user_config_path_backup = os.path.expanduser(
+    user_config_path = os.path.expanduser('~/.cookiecutterrc')
+    user_config_path_backup = os.path.expanduser(
         '~/.cookiecutterrc.backup'
     )
 
-    if os.path.exists(self.user_config_path):
-        shutil.copy(self.user_config_path, self.user_config_path_backup)
-        os.remove(self.user_config_path)
+    if os.path.exists(user_config_path):
+        shutil.copy(user_config_path, user_config_path_backup)
+        os.remove(user_config_path)
 
     def restore_rc():
         """
         If it existed, restore ~/.cookiecutterrc
         """
-        if os.path.exists(self.user_config_path_backup):
-            shutil.copy(self.user_config_path_backup, self.user_config_path)
-            os.remove(self.user_config_path_backup)
+        if os.path.exists(user_config_path_backup):
+            shutil.copy(user_config_path_backup, user_config_path)
+            os.remove(user_config_path_backup)
     request.addfinalizer(restore_rc)
