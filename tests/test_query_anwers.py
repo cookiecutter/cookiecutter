297e2d0901f73e573e26c427cbf1ddc2179ca0b4
Raphael Pierzina <raphael@hackebrot.de>
2014-11-30 23:48:08 +0100

    Reimplement test_query_y using python

---
+++ tests/test_query_anwers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_query_anwers
+-----------------
+
+Tests formerly known from a unittest residing in test_prompt.py named
+TestQueryAnswers.test_query_y
+"""
+
+import pytest
+
+
+def test_query_y(monkeypatch):
+    monkeypatch.setattr(
+        'cookiecutter.prompt.read_response',
+        lambda x=u'': u'y'
+    )
+    answer = prompt.query_yes_no("Blah?")
+    assert answer
