5d63102f6e92eb5eb9b73da0133616b18ec6ae6e
Daniel Greenfeld <danny@eventbrite.com>
2015-01-13 15:20:42 -0800

    flake8 fix

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -12,4 +12,5 @@ from .compat import OLD_PY2
 __version__ = '0.9.0'
 
 if OLD_PY2:
-    raise DeprecationWarning('Python 2.6 support was removed from cookiecutter in release 1.0.0.')
+    msg = 'Python 2.6 support was removed from cookiecutter in release 1.0.0.'
+    raise DeprecationWarning(msg)
