424fb80d4922f7b222daeb698ab8136d543d14d0
Jace Browning <jacebrowning@gmail.com>
2015-01-19 17:25:45 -0500

    Simplify string detection
    Conflicts:
    	cookiecutter/compat.py

--- cookiecutter/compat.py
+++ cookiecutter/compat.py
@@ -67,17 +67,82 @@ else:  # pragma: no cover
         enc = sys.stdin.encoding or sys.getdefaultencoding()
         return raw_input(prompt).decode(enc)
 
-    try:
-        isinstance("", basestring)
+if PY3:  # Forced testing
 
-        def isstr(s):
-            """Determine if an object is a string in Python 2."""
-            return isinstance(s, basestring)
+    from shutil import which
 
-    except NameError:
+else:  # Forced testing
 
-        def isstr(s):
-            """Determine if an object is a string in Python 3."""
-            return isinstance(s, str)
+    def is_exe(program):
+        """
+        Returns whether or not a file is an executable.
+        """
+        return os.path.isfile(program) and os.access(program, os.X_OK)
+
+    def which(cmd, mode=os.F_OK | os.X_OK, path=None):
+        """Given a command, mode, and a PATH string, return the path which
+        conforms to the given mode on the PATH, or None if there is no such
+        file.
+        `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+        of os.environ.get("PATH"), or can be overridden with a custom search
+        path.
+
+        Note: This function was backported from the Python 3 source code.
+        """
+        # Check that a given file can be accessed with the correct mode.
+        # Additionally check that `file` is not a directory, as on Windows
+        # directories pass the os.access check.
+        def _access_check(fn, mode):
+            return (os.path.exists(fn) and os.access(fn, mode)
+                    and not os.path.isdir(fn))
+
+        # If we're given a path with a directory part, look it up directly
+        # rather than referring to PATH directories. This includes checking
+        # relative to the current directory, e.g. ./script
+        if os.path.dirname(cmd):
+            if _access_check(cmd, mode):
+                return cmd
+            return None
+
+        if path is None:
+            path = os.environ.get("PATH", os.defpath)
+        if not path:
+            return None
+        path = path.split(os.pathsep)
+
+        if sys.platform == "win32":
+            # The current directory takes precedence on Windows.
+            if os.curdir not in path:
+                path.insert(0, os.curdir)
+
+            # PATHEXT is necessary to check on Windows.
+            pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+            # See if the given file matches any of the expected path
+            # extensions. This will allow us to short circuit when given
+            # "python.exe". If it does match, only test that one, otherwise we
+            # have to try others.
+            if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+                files = [cmd]
+            else:
+                files = [cmd + ext for ext in pathext]
+        else:
+            # On other platforms you don't have things like PATHEXT to tell you
+            # what file suffixes are executable, so just pass on cmd as-is.
+            files = [cmd]
+
+        seen = set()
+        for dir in path:
+            normdir = os.path.normcase(dir)
+            if normdir not in seen:
+                seen.add(normdir)
+                for thefile in files:
+                    name = os.path.join(dir, thefile)
+                    if _access_check(name, mode):
+                        return name
+        return None
+
+def isstr(obj):
+    """Determine if an object is a string."""
+    return isinstance(obj, str if PY3 else basestring)  # NOQA
 
 _hush_pyflakes = (patch, StringIO, json, OrderedDict, unittest)
