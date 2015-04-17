ec5b78071020966413d1629ef773730598bbea00
Raphael Pierzina <raphael@hackebrot.de>
2015-04-17 09:01:21 +0200

    Add doc str and test for read_choice click.prompt parameters

--- tests/test_read_choice.py
+++ tests/test_read_choice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import click
 import pytest
 
 from cookiecutter.compat import read_choice
@@ -18,8 +19,14 @@ Choose from 1, 2, 3, 4!"""
 
 @pytest.mark.parametrize('user_choice, expected_value', enumerate(OPTIONS, 1))
 def test_read_choice(monkeypatch, user_choice, expected_value):
-    def _monkey_prompt(prompt, **kwargs):
+    """Make sure that ``click.prompt`` is being called with the desired prompt
+    and choice type. Patch it so that it returns str value of the
+    ``user_choice`` fixture.
+    """
+    def _monkey_prompt(prompt, type=None):
         assert prompt == EXPECTED_PROMPT
+        assert isinstance(type, click.Choice)
+        assert type.choices == ['1', '2', '3', '4']
         return str(user_choice)
     monkeypatch.setattr('click.prompt', _monkey_prompt)
 
