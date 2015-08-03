9b12a9cdab0021fea7e5f2d8fd8ffe11d065f0d0
Raphael Pierzina <raphael@hackebrot.de>
2015-08-04 00:42:50 +0200

    Implement tests for replay.dump args

--- tests/test_replay.py
+++ tests/test_replay.py
@@ -5,6 +5,13 @@ test_replay
 -----------
 """
 
+import os
+import pytest
+
+
+from cookiecutter import replay
+from cookiecutter.config import get_user_config
+
 
 def test_get_user_config():
     config_dict = get_user_config()
@@ -12,3 +19,28 @@ def test_get_user_config():
 
     expected_dir = os.path.expanduser('~/.cookiecutter_replay/')
     assert config_dict['replay_dir'] == expected_dir
+
+
+@pytest.fixture
+def template_name():
+    return 'cookiedozer'
+
+
+@pytest.fixture
+def context():
+    return {
+        u'email': u'raphael@hackebrot.de',
+        u'full_name': u'Raphael Pierzina',
+        u'github_username': u'hackebrot',
+        u'version': u'0.1.0',
+    }
+
+
+def test_dump_value_error_if_no_template_name(context):
+    with pytest.raises(ValueError):
+        replay.dump(None, context)
+
+
+def test_dump_type_error_if_not_dict_context(template_name):
+    with pytest.raises(TypeError):
+        replay.dump(template_name, 'not_a_dict')
