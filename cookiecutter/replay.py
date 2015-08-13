faa9f6fee3ad5e55915a65ee1e332dade7900cf4
Raphael Pierzina <raphael@hackebrot.de>
2015-08-14 00:46:38 +0200

    Retrieve replay_dir in replay.dump and create try to create it

--- cookiecutter/replay.py
+++ cookiecutter/replay.py
@@ -8,6 +8,8 @@ cookiecutter.replay
 from __future__ import unicode_literals
 
 from .compat import is_string
+from .config import get_user_config
+from .utils import make_sure_path_exists
 
 
 def dump(template_name, context):
@@ -16,3 +18,8 @@ def dump(template_name, context):
 
     if not isinstance(context, dict):
         raise TypeError('Context is required to be of type dict')
+
+    replay_dir = get_user_config()['replay_dir']
+
+    if not make_sure_path_exists(replay_dir):
+        raise IOError('Unable to create replay dir at {}'.format(replay_dir))
