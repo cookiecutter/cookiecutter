b6b627cb4c5d6b7dc1636794de870a2bf6da262b
Raphael Pierzina <raphael@hackebrot.de>
2015-08-14 00:32:41 +0200

    Raise a TypeError if context is not a dict

--- cookiecutter/replay.py
+++ cookiecutter/replay.py
@@ -13,3 +13,6 @@ from .compat import is_string
 def dump(template_name, context):
     if not is_string(template_name):
         raise TypeError('Template name is required to be of type str')
+
+    if not isinstance(context, dict):
+        raise TypeError('Context is required to be of type dict')
