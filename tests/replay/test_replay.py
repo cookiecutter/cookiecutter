cb08d25f49b8b4c5177c8afdd9a69330992ee854
Raphael Pierzina <raphael@hackebrot.de>
2015-08-16 21:56:55 +0200

    Add tests for a correct behaviour in cookiecutter.main for replay

--- tests/replay/test_replay.py
+++ tests/replay/test_replay.py
@@ -27,3 +27,35 @@ def invalid_kwargs(request):
 def test_raise_on_invalid_mode(invalid_kwargs):
     with pytest.raises(exceptions.InvalidModeException):
         main.cookiecutter('foo', replay=True, **invalid_kwargs)
+
+
+def test_main_does_not_invoke_dump_but_load(mocker):
+    mock_prompt = mocker.patch('cookiecutter.main.prompt_for_config')
+    mock_gen_context = mocker.patch('cookiecutter.main.generate_context')
+    mock_gen_files = mocker.patch('cookiecutter.main.generate_files')
+    mock_replay_dump = mocker.patch('cookiecutter.main.dump')
+    mock_replay_load = mocker.patch('cookiecutter.main.load')
+
+    main.cookiecutter('foobar', replay=True)
+
+    assert not mock_prompt.called
+    assert not mock_gen_context.called
+    assert not mock_replay_dump.called
+    assert mock_replay_load.called
+    assert mock_gen_files.called
+
+
+def test_main_does_not_invoke_load_but_dump(mocker):
+    mock_prompt = mocker.patch('cookiecutter.main.prompt_for_config')
+    mock_gen_context = mocker.patch('cookiecutter.main.generate_context')
+    mock_gen_files = mocker.patch('cookiecutter.main.generate_files')
+    mock_replay_dump = mocker.patch('cookiecutter.main.dump')
+    mock_replay_load = mocker.patch('cookiecutter.main.load')
+
+    main.cookiecutter('foobar', replay=False)
+
+    assert mock_prompt.called
+    assert mock_gen_context.called
+    assert mock_replay_dump.called
+    assert not mock_replay_load.called
+    assert mock_gen_files.called
