e8b2a06cfa026e07ff02940073abfaccb42e42e3
Charles-Axel Dein <charles@uber.com>
2015-08-19 15:27:08 +0200

    Add test for mercurial

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,6 +6,7 @@ def test_is_repo_url():
     assert is_repo_url('gitolite@server:team/repo') is True
     assert is_repo_url('git@github.com:audreyr/cookiecutter.git') is True
     assert is_repo_url('https://github.com/audreyr/cookiecutter.git') is True
+    assert is_repo_url('gh:audreyr/cookiecutter-pypackage') is True
 
     assert is_repo_url('/audreyr/cookiecutter.git') is False
     assert is_repo_url('/home/audreyr/cookiecutter') is False
