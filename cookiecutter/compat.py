9939e2e1a01092e2167fc6fc581bbe51f9702433
Raphael Pierzina <raphael@hackebrot.de>
2015-09-24 20:25:37 +0200

    Remove unused input_str from compat

--- cookiecutter/compat.py
+++ cookiecutter/compat.py
@@ -5,14 +5,10 @@ OLD_PY2 = sys.version_info[:2] < (2, 7)
 
 
 if PY3:  # pragma: no cover
-    input_str = 'builtins.input'
     iteritems = lambda d: iter(d.items())
 
 
 else:  # pragma: no cover
-    from __builtin__ import raw_input
-    input = raw_input
-    input_str = '__builtin__.raw_input'
     iteritems = lambda d: d.iteritems()
 
 
