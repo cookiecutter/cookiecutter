757ff60be8dbca213b55d625b98277ab8e5cd6c6
Raphael Pierzina <raphael@hackebrot.de>
2015-09-26 21:55:43 +0200

    Remove warning for Python 2.6 from __init__.py

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -7,10 +7,5 @@ cookiecutter
 
 Main package for Cookiecutter.
 """
-import sys
 
 __version__ = '1.0.0'
-
-if sys.version_info[:2] < (2, 7):
-    msg = 'Python 2.6 support was removed from cookiecutter in release 1.0.0.'
-    raise DeprecationWarning(msg)
