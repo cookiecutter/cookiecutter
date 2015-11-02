d085ae9952684b63091380acf6d7944718a98569
Raphael Pierzina <raphael@hackebrot.de>
2015-11-02 12:10:17 +0100

    Implement a test for non-nested values

--- tests/test_parse_user_config.py
+++ tests/test_parse_user_config.py
@@ -40,3 +40,12 @@ def expected_config():
 
 def test_parse_config(user_config, expected_config):
     assert config_parser.loads(user_config) == expected_config
+
+
+def test_find_simple(user_config):
+    result = config_parser.find_simple(user_config)
+    expected = {
+        'cookiecutters_dir': "/home/audreyr/my-custom-cookiecutters-dir/",
+        'replay_dir': "/home/audreyr/my-custom-replay-dir/",
+    }
+    assert result == expected
