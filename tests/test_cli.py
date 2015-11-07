0c0de157f51a699c26e8fc0be36beeccb12f4efc
Raphael Pierzina <raphael@hackebrot.de>
2015-11-07 14:58:14 +0100

    Implement a test for --default-config only

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -272,3 +272,26 @@ def test_default_user_config_overwrite(mocker, user_config_path):
         output_dir='.',
         config_file=None
     )
+
+
+def test_default_user_config(mocker):
+    mock_cookiecutter = mocker.patch(
+        'cookiecutter.cli.cookiecutter'
+    )
+
+    template_path = 'tests/fake-repo-pre/'
+    result = runner.invoke(main, [
+        template_path,
+        '--default-config'
+    ])
+
+    assert result.exit_code == 0
+    mock_cookiecutter.assert_called_once_with(
+        template_path,
+        None,
+        False,
+        replay=False,
+        overwrite_if_exists=False,
+        output_dir='.',
+        config_file=None
+    )
