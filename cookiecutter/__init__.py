69e0fb66274ef7e92bcdc4c1f5010b984ac3e2f2
Raphael Pierzina <raphael@hackebrot.de>
2015-11-10 23:47:32 +0100

    Bump version: 1.2.1 → 1.3.0

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -8,4 +8,4 @@ cookiecutter
 Main package for Cookiecutter.
 """
 
-__version__ = '1.2.1'
+__version__ = '1.3.0'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-version = "1.2.1"
+version = "1.3.0"
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
