2d7db7a3cc35b0425e282787b0b2faf591314b06
Raphael Pierzina <raphael@hackebrot.de>
2015-11-13 23:31:50 +0100

    Update replay/test_dump.py to pass in replay dir

--- tests/replay/test_dump.py
+++ tests/replay/test_dump.py
@@ -34,24 +34,24 @@ def remove_replay_dump(request, replay_file):
     request.addfinalizer(fin_remove_replay_file)
 
 
-def test_type_error_if_no_template_name(context):
+def test_type_error_if_no_template_name(replay_test_dir, context):
     """Test that replay.dump raises if the tempate_name is not a valid str."""
     with pytest.raises(TypeError):
-        replay.dump(None, context)
+        replay.dump(replay_test_dir, None, context)
 
 
-def test_type_error_if_not_dict_context(template_name):
+def test_type_error_if_not_dict_context(replay_test_dir, template_name):
     """Test that replay.dump raises if the context is not of type dict."""
     with pytest.raises(TypeError):
-        replay.dump(template_name, 'not_a_dict')
+        replay.dump(replay_test_dir, template_name, 'not_a_dict')
 
 
-def test_value_error_if_key_missing_in_context(template_name):
+def test_value_error_if_key_missing_in_context(replay_test_dir, template_name):
     """Test that replay.dump raises if the context does not contain a key
     named 'cookiecutter'.
     """
     with pytest.raises(ValueError):
-        replay.dump(template_name, {'foo': 'bar'})
+        replay.dump(replay_test_dir, template_name, {'foo': 'bar'})
 
 
 @pytest.fixture
@@ -71,11 +71,14 @@ def mock_ensure_success(mocker):
 
 
 def test_ioerror_if_replay_dir_creation_fails(
-        mock_ensure_failure, mock_user_config, replay_test_dir):
+        mock_ensure_failure, replay_test_dir):
     """Test that replay.dump raises when the replay_dir cannot be created."""
 
     with pytest.raises(IOError):
-        replay.dump('foo', {'cookiecutter': {'hello': 'world'}})
+        replay.dump(
+            replay_test_dir,
+            'foo', {'cookiecutter': {'hello': 'world'}}
+        )
 
     mock_ensure_failure.assert_called_once_with(replay_test_dir)
 
@@ -89,9 +92,9 @@ def test_run_json_dump(mocker, mock_ensure_success, mock_user_config,
 
     mock_json_dump = mocker.patch('json.dump', side_effect=json.dump)
 
-    replay.dump(template_name, context)
+    replay.dump(replay_test_dir, template_name, context)
 
-    assert mock_user_config.call_count == 1
+    assert not mock_user_config.called
     mock_ensure_success.assert_called_once_with(replay_test_dir)
     spy_get_replay_file.assert_called_once_with(replay_test_dir, template_name)
 
