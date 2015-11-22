9d57f85837d577e733cf1a45e560fc763a62be1f
Raphael Pierzina <raphael@hackebrot.de>
2015-11-22 02:28:44 +0100

    Implement a custom exception type for undefined variable errors

--- cookiecutter/exceptions.py
+++ cookiecutter/exceptions.py
@@ -87,3 +87,20 @@ class FailedHookException(CookiecutterException):
     """
     Raised when a hook script fails
     """
+
+
+class UndefinedVariableInTemplate(CookiecutterException):
+    """Raised when a template uses a variable which is not defined in the
+    context.
+    """
+    def __init__(self, message, error, context):
+        self.message = message
+        self.error = error
+        self.context = context
+
+    def __str__(self):
+        return (
+            "{self.message}. "
+            "Error message: {self.error.message}. "
+            "Context: {self.context}"
+        ).format(**locals())
