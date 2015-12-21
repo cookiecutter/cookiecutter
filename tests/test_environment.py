2ce4386082773652a6da41c5f776882a77ad96ca
Raphael Pierzina <raphael@hackebrot.de>
2015-12-21 01:03:23 +0100

    Implement a basic test for StrictEnvironment

---
+++ tests/test_environment.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from cookiecutter.environment import StrictEnvironment
+from cookiecutter.exceptions import UnknownExtension
+
+
+def test_env_should_raise_for_unknown_extension():
+    context = {
+        '_extensions': ['foobar']
+    }
+
+    with pytest.raises(UnknownExtension) as err:
+        StrictEnvironment(context, keep_trailing_newline=True)
+
+    assert 'Unable to load extension: No module named foobar' in str(err.value)
