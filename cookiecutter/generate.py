a262a0f2d078e7b25ec15911ba8b2d1bea036d0d
Raphael Pierzina <raphael@hackebrot.de>
2016-01-31 02:04:04 +0000

    Replace jinja2.Environment with StrictEnvironment

--- cookiecutter/generate.py
+++ cookiecutter/generate.py
@@ -16,8 +16,8 @@ import logging
 import os
 import shutil
 
-from jinja2 import FileSystemLoader, StrictUndefined
-from jinja2.environment import Environment
+from jinja2 import FileSystemLoader
+from cookiecutter.environment import StrictEnvironment
 from jinja2.exceptions import TemplateSyntaxError, UndefinedError
 from binaryornot.check import is_binary
 
@@ -256,10 +256,7 @@ def generate_files(repo_dir, context=None, output_dir='.',
 
     unrendered_dir = os.path.split(template_dir)[1]
     ensure_dir_is_templated(unrendered_dir)
-    env = Environment(
-        keep_trailing_newline=True,
-        undefined=StrictUndefined
-    )
+    env = StrictEnvironment(keep_trailing_newline=True)
     try:
         project_dir = render_and_create_dir(
             unrendered_dir,
