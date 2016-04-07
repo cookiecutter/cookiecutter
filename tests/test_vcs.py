14bdff80ae57d6d0b9229d481b47396e96ac9bed
Raphael Pierzina <raphael@hackebrot.de>
2016-04-07 19:49:11 +0100

    Implement parametrized tests for vcs.is_vcs_installed

--- tests/test_vcs.py
+++ tests/test_vcs.py
@@ -128,6 +128,21 @@ def test_clone_should_raise_if_vcs_not_installed(mocker, clone_dir):
         vcs.clone(repo_url, clone_to_dir=clone_dir)
 
 
+@pytest.mark.parametrize('which_return, result', [
+    ('', False),
+    (None, False),
+    (False, False),
+    ('/usr/local/bin/git', True),
+])
+def test_is_vcs_installed(mocker, which_return, result):
+    mocker.patch(
+        'cookiecutter.vcs.which',
+        autospec=True,
+        return_value=which_return
+    )
+    assert vcs.is_vcs_installed('git') == result
+
+
 @pytest.mark.parametrize('repo_type, repo_url, repo_name', [
     ('git', 'https://github.com/hello/world.git', 'world'),
     ('hg', 'https://bitbucket.org/foo/bar', 'bar'),
