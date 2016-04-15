9b7b76a7d4c3450a92323452a940c53b9a8bd500
Michael Joseph <michaeljoseph+github@gmail.com>
2016-04-15 13:34:44 +0200

    Prefer string interpolation with format and single quotes

--- cookiecutter/cli.py
+++ cookiecutter/cli.py
@@ -42,9 +42,9 @@ def validate_extra_context(ctx, param, value):
     for s in value:
         if '=' not in s:
             raise click.BadParameter(
-                "EXTRA_CONTEXT should contain items of the form key=value; "
-                "'%s' doesn't match that form"
-                % (s,))
+                'EXTRA_CONTEXT should contain items of the form key=value; '
+                "'{}' doesn't match that form".format(s)
+            )
 
     # Convert tuple -- e.g.: (u'program_name=foobar', u'startsecs=66')
     # to dict -- e.g.: {'program_name': 'foobar', 'startsecs': '66'}
