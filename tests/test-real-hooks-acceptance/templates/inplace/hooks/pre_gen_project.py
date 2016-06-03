5a29c4c75b9905a5311297382edc5168c404b7cb
Eric Villard <dev@eviweb.fr>
2016-06-03 23:57:26 +0200

    fix E402 module level import not at top of file in a test hook file
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- tests/test-real-hooks-acceptance/templates/inplace/hooks/pre_gen_project.py
+++ tests/test-real-hooks-acceptance/templates/inplace/hooks/pre_gen_project.py
@@ -4,10 +4,11 @@
 import sys
 import os
 
+from support import create_file
+
 hooks_dir = os.path.dirname(__file__)
 sys.path.append(hooks_dir)
 
-from support import create_file
 
 template_dir = os.path.dirname(hooks_dir)
 create_file('inplace', template_dir)
