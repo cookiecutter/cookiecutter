32162c5b5c67369418070cc042311c4cf5d0b4a4
Eric Villard <dev@eviweb.fr>
2016-06-07 11:28:55 +0200

    refactor acceptance tests
    extract all common behaviours to support.AbstractAcceptanceTest class
    it is the base class to write acceptance tests
    
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- tests/support.py
+++ tests/support.py
@@ -7,8 +7,24 @@ support
 
 Add test support utilities which are globally available throughout the suite.
 """
+import os
+import tempfile
 
+from abc import ABCMeta, abstractmethod
 from cookiecutter.main import cookiecutter
+from cookiecutter.utils import rmtree
+
+
+def read_file(file):
+    """
+    helper method to get the content of a given file
+    :param file: the file to read
+    """
+    fd = open(file, 'r')
+    content = fd.read()
+    fd.close()
+
+    return content
 
 
 class Runner(object):
@@ -62,3 +78,201 @@ class SettingObject(object):
         self.overwrite_if_exists = False
         self.output_dir = output_dir
         self.config_file = config_file
+
+
+class AbstractAcceptanceTest(object):
+    """
+    abstract base class to create acceptance tests
+
+    to use this class, after having imported it :
+    1. create a test class that inherits from this abstract class
+    2. implement the abstract method _repo_id
+    3. write your test methods as usual and consume the public api of
+    this class
+
+    eq.
+    from .support import AbstractAcceptanceTest
+
+    class TestFeatureAcceptance(AbstractAcceptanceTest):
+        def _repo_id(self):
+            # this means fixtures are set under ./tests/test-feature-acceptance
+            return 'test-feature-acceptance'
+
+        def test_some_behaviour(self):
+            self.run('template')
+            assert my_expected_behaviour_of_my_template
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def _repo_id(self):
+        """
+        abstract property to be implemented by acceptance tests
+        it should return the directory basename in which are set the templates
+        used to build the tests
+        ie. return 'test-feature-acceptance'
+        """
+
+    def setup_method(self, method):
+        """
+        setup method
+        take care of calling the super().setup_method() in case of overwriting
+        """
+        self.__setup()
+        self.__reset_runtime()
+        self.__prepare_test_environment()
+
+    def teardown_method(self, method):
+        """
+        teardown method
+        take care of calling the super().teardown_method() in case of
+        overwriting
+        """
+        self.__reset_runtime()
+        self.__cleanup_test_environment()
+
+    @property
+    def repo_path(self):
+        """
+        path of the test repository in which are set test fixtures
+        read-only
+        """
+        return os.path.join(self.__tests_dir, self._repo_id())
+
+    @property
+    def user_config(self):
+        """
+        user configuration file
+        taken from repo_path if it exists or from the main fixtures directory
+        by default
+        it has to be named: cookiecutterrc
+        read-only
+        """
+        name = 'cookiecutterrc'
+        rcfile = os.path.join(self.repo_path, name)
+
+        return rcfile if os.path.exists(rcfile) else os.path.join(
+            self.__tests_dir, 'fixtures', name)
+
+    @property
+    def templates_path(self):
+        """
+        path of the directory where lie templates fixtures
+        read-only
+        """
+        return os.path.join(self.repo_path, 'templates')
+
+    @property
+    def project(self):
+        """
+        project name
+        """
+        return self.__project
+
+    @project.setter
+    def project(self, name):
+        """
+        project name setter
+        :param name: name of the project
+        """
+        self.__project = name
+
+    @property
+    def project_dir(self):
+        """
+        output directory of the current project
+        its name is automatically generated from the project name and is
+        created under the output_dir
+        read-only
+        """
+        return os.path.join(
+            self.output_dir,
+            self.project.strip().lower().replace(' ', '-')
+        )
+
+    @property
+    def output_dir(self):
+        """
+        output diretory where the project under test is created
+        read-only
+        """
+        if not self.__output_dir:
+            self.__output_dir = tempfile.mkdtemp()
+
+        return self.__output_dir
+
+    @property
+    def runner(self):
+        """
+        current cookiecutter runner instance
+        read-only
+        """
+        return self.__runner
+
+    @property
+    def settings(self):
+        """
+        current cookiecutter runner configuration object
+        read-only
+        """
+        return self.__settings
+
+    def configure(self, template, extra_context=None, config_file=None):
+        """
+        configure the runtime environment
+        :param template: name of the template
+        :param extra_context: extra context dictionary
+        :param config_file: path to the user cookiecutterrc file
+        """
+        template_dir = os.path.join(self.templates_path, template)
+        assert os.path.exists(template_dir)
+        context = {"project_name": self.project} if not extra_context \
+            else extra_context
+        config = self.user_config if not config_file else config_file
+
+        self.__settings = SettingObject(
+            template_dir, config, context, self.output_dir)
+        self.__runner = Runner(self.settings)
+
+    def run(self, template=None):
+        """
+        run cookiecutter
+        :param template: template name if a default configuration is sufficient
+        """
+        if template:
+            self.configure(template)
+        assert self.runner
+        self.runner.run()
+
+    ###############
+    # PRIVATE API #
+    ###############
+
+    def __setup(self):
+        """
+        helper method to initialize the test class internals
+        """
+        self.__tests_dir = os.path.abspath('tests')
+        self.__output_dir = None
+        self.project = 'Dummy Project'
+
+    def __reset_runtime(self):
+        """
+        helper method to reset the runtime environment
+        """
+        self.__runner = None
+        self.__settings = None
+
+    def __prepare_test_environment(self):
+        """
+        helper method to prepare the test environment
+        """
+        assert os.path.exists(self.output_dir)
+
+    def __cleanup_test_environment(self):
+        """
+        helper method to clean up the test environment
+        """
+        rmtree(self.output_dir)
+        assert not os.path.exists(self.output_dir)
+        self.__output_dir = None
--- tests/test_real_hooks_acceptance.py
+++ tests/test_real_hooks_acceptance.py
@@ -10,72 +10,15 @@ Additional tests for `cookiecutter.hooks` module.
 
 import os
 import sys
-import tempfile
-
-from cookiecutter.utils import rmtree
-from .support import SettingObject, Runner
 
 
-class TestRealHooksAcceptance(object):
-    repo_path = os.path.abspath(
-        'tests/test-real-hooks-acceptance')
-    templates_path = os.path.join(repo_path, 'templates')
-    rcfile = os.path.join(repo_path, 'cookiecutterrc')
-    default_project = 'Dummy Project'
-    default_project_dir = 'dummy-project'
+from .support import read_file, AbstractAcceptanceTest
 
-    def setup_method(self, method):
-        self.runner = None
-        self.settings = None
-        self.__prepare_test_environment()
 
-    def teardown_method(self, method):
-        self.runner = None
-        self.settings = None
-        self.__cleanup_test_environment()
+class TestRealHooksAcceptance(AbstractAcceptanceTest):
 
-    def __prepare_test_environment(self):
-        """
-        helper method to prepare the test environment
-        """
-        self.output_dir = tempfile.mkdtemp()
-        assert os.path.exists(self.output_dir)
-        self.project_dir = os.path.join(
-            self.output_dir, self.default_project_dir)
-
-    def __cleanup_test_environment(self):
-        """
-        helper method to clean up the test environment
-        """
-        rmtree(self.output_dir)
-        assert not os.path.exists(self.output_dir)
-
-    def configure(self, template, extra_context=None, config_file=None):
-        """
-        configure the runtime environment
-        :param template: name of the template
-        :param extra_context: extra context dictionary
-        :param config_file: path to the user cookiecutterrc file
-        """
-        template_dir = os.path.join(self.templates_path, template)
-        assert os.path.exists(template_dir)
-        context = {"project_name": self.default_project} if not extra_context \
-            else extra_context
-        config = self.rcfile if not config_file else config_file
-
-        self.settings = SettingObject(
-            template_dir, config, context, self.output_dir)
-        self.runner = Runner(self.settings)
-
-    def run(self, template=None):
-        """
-        run cookiecutter
-        :param template: template name if a default configuration is sufficient
-        """
-        if template:
-            self.configure(template)
-        assert self.runner
-        self.runner.run()
+    def _repo_id(self):
+        return 'test-real-hooks-acceptance'
 
     def assert_real_hook_is_run_in_place(self, template):
         """
@@ -86,7 +29,7 @@ class TestRealHooksAcceptance(object):
         file = os.path.join(self.project_dir, template)
         self.run(template)
         assert os.path.exists(file)
-        content = open(file, 'r').read()
+        content = read_file(file)
         assert template_dir == content.strip()
 
     def test_renderable_hooks_can_be_run(self):
