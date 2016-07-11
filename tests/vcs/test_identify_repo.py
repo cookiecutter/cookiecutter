ec57ccd3f58e9d1428269d6e2ea6832b5412520c
michaeljoseph <michaeljoseph+github@gmail.com>
2016-07-11 22:53:00 +0200

    Move standalone identify_repo test cases into parameterised test

--- tests/vcs/test_identify_repo.py
+++ tests/vcs/test_identify_repo.py
@@ -6,21 +6,37 @@ from cookiecutter import exceptions, vcs
 
 @pytest.mark.parametrize('repo_url, exp_repo_type, exp_repo_url', [
     (
-        "git+https://github.com/pytest-dev/cookiecutter-pytest-plugin.git",
-        "git",
-        "https://github.com/pytest-dev/cookiecutter-pytest-plugin.git"
+        'git+https://github.com/pytest-dev/cookiecutter-pytest-plugin.git',
+        'git',
+        'https://github.com/pytest-dev/cookiecutter-pytest-plugin.git'
     ), (
-        "hg+https://bitbucket.org/foo/bar.hg",
-        "hg",
-        "https://bitbucket.org/foo/bar.hg"
+        'hg+https://bitbucket.org/foo/bar.hg',
+        'hg',
+        'https://bitbucket.org/foo/bar.hg'
     ), (
-        "https://github.com/pytest-dev/cookiecutter-pytest-plugin.git",
-        "git",
-        "https://github.com/pytest-dev/cookiecutter-pytest-plugin.git"
+        'https://github.com/pytest-dev/cookiecutter-pytest-plugin.git',
+        'git',
+        'https://github.com/pytest-dev/cookiecutter-pytest-plugin.git'
     ), (
-        "https://bitbucket.org/foo/bar.hg",
-        "hg",
-        "https://bitbucket.org/foo/bar.hg"
+        'https://bitbucket.org/foo/bar.hg',
+        'hg',
+        'https://bitbucket.org/foo/bar.hg'
+    ), (
+        'https://github.com/audreyr/cookiecutter-pypackage.git',
+        'git',
+        'https://github.com/audreyr/cookiecutter-pypackage.git',
+    ), (
+        'https://github.com/audreyr/cookiecutter-pypackage',
+        'git',
+        'https://github.com/audreyr/cookiecutter-pypackage',
+    ), (
+        'git@gitorious.org:cookiecutter-gitorious/cookiecutter-gitorious.git',
+        'git',
+        'git@gitorious.org:cookiecutter-gitorious/cookiecutter-gitorious.git',
+    ), (
+        'https://audreyr@bitbucket.org/audreyr/cookiecutter-bitbucket',
+        'hg',
+        'https://audreyr@bitbucket.org/audreyr/cookiecutter-bitbucket',
     )
 ])
 def test_identify_known_repo(repo_url, exp_repo_type, exp_repo_url):
@@ -28,10 +44,11 @@ def test_identify_known_repo(repo_url, exp_repo_type, exp_repo_url):
 
 
 @pytest.fixture(params=[
-    "foo+git",  # uses explicit identifier with 'git' in the wrong place
-    "foo+hg",  # uses explicit identifier with 'hg' in the wrong place
-    "foo+bar",  # uses explicit identifier with neither 'git' nor 'hg'
-    "foobar"  # no identifier but neither 'git' nor 'bitbucket' in url
+    'foo+git',  # uses explicit identifier with 'git' in the wrong place
+    'foo+hg',  # uses explicit identifier with 'hg' in the wrong place
+    'foo+bar',  # uses explicit identifier with neither 'git' nor 'hg'
+    'foobar',  # no identifier but neither 'git' nor 'bitbucket' in url
+    'http://norepotypespecified.com'
 ])
 def unknown_repo_type_url(request):
     return request.param
