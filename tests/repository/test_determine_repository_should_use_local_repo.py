e5b628a1df38e31db456908e84f886793eccfcb2
michaeljoseph <michaeljoseph+github@gmail.com>
2016-07-13 18:27:49 +0200

    Test local repository validation

--- tests/repository/test_determine_repository_should_use_local_repo.py
+++ tests/repository/test_determine_repository_should_use_local_repo.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from cookiecutter.repository import determine_repo_dir
+from cookiecutter import repository, exceptions
+
+import pytest
 
 
-def test_determine_repository_should_use_local_repo():
-    project_dir = determine_repo_dir(
+def test_finds_local_repo():
+    project_dir = repository.determine_repo_dir(
         'tests/fake-repo',
         abbreviations={},
         clone_to_dir=None,
@@ -12,3 +14,17 @@ def test_determine_repository_should_use_local_repo():
     )
 
     assert 'tests/fake-repo' == project_dir
+
+
+def test_local_repo_with_no_context_raises(user_config_data):
+    """A repository without a cookiecutter.json should raise a
+    `RepositoryNotFound` exception.
+    """
+    with pytest.raises(exceptions.RepositoryNotFound):
+        repository.determine_repo_dir(
+            'tests/fake-repo-bad',
+            abbreviations={},
+            clone_to_dir=user_config_data['cookiecutters_dir'],
+            checkout=None,
+            no_input=True
+        )
