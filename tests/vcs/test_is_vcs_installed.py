ad4a9a628958917c3ef849ea1c6e3940c2df1435
michaeljoseph <michaeljoseph+github@gmail.com>
2016-07-14 21:05:06 +0200

    Fix test refactor

--- tests/vcs/test_is_vcs_installed.py
+++ tests/vcs/test_is_vcs_installed.py
@@ -1,31 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from cookiecutter import exceptions, vcs
-
-
-@pytest.fixture
-def clone_dir(tmpdir):
-    """Simulates creation of a directory called `clone_dir` inside of `tmpdir`.
-    Returns a str to said directory.
-    """
-    return str(tmpdir.mkdir('clone_dir'))
-
-
-def test_clone_should_raise_if_vcs_not_installed(mocker, clone_dir):
-    """In `clone()`, a `VCSNotInstalled` exception should be raised if no VCS
-    is installed.
-    """
-    mocker.patch(
-        'cookiecutter.vcs.is_vcs_installed',
-        autospec=True,
-        return_value=False
-    )
-
-    repo_url = 'https://github.com/pytest-dev/cookiecutter-pytest-plugin.git'
-
-    with pytest.raises(exceptions.VCSNotInstalled):
-        vcs.clone(repo_url, clone_to_dir=clone_dir)
+from cookiecutter import vcs
 
 
 @pytest.mark.parametrize('which_return, result', [
