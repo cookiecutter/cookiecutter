20e5093a541e2b5c7db168121dcda29920b3d4ff
Brett Cannon <brett@python.org>
2016-07-29 14:27:36 -0700

    Fix program name when using __main__

--- cookiecutter/__main__.py
+++ cookiecutter/__main__.py
@@ -4,5 +4,5 @@ from __future__ import absolute_import
 from .cli import main
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":  # pragma: no cover
+    main(prog_name="cookiecutter")
