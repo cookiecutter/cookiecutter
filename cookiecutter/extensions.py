a1ad9f13aba0eed53257024bac2e8bd481a43fa9
Raphael Pierzina <raphael@hackebrot.de>
2016-12-16 15:35:32 +0000

    Sort imports based on pep8 in extensions.py

--- cookiecutter/extensions.py
+++ cookiecutter/extensions.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+
 """Jinja2 extensions."""
 
-from jinja2.ext import Extension
 import json
+
+from jinja2.ext import Extension
 
 
 class JsonifyExtension(Extension):
     """Jinja2 extension to convert a python object to json"""
+
     def __init__(self, environment):
         super(JsonifyExtension, self).__init__(environment)
 
