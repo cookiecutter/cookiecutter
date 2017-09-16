0e152cee7876347d0029730e05698eb2f6137479
Russell Keith-Magee <russell@keith-magee.com>
2017-09-16 19:14:35 +0800

    Minor change for Windows compatibility.

--- cookiecutter/zipfile.py
+++ cookiecutter/zipfile.py
@@ -66,7 +66,7 @@ def unzip(zip_uri, is_url, clone_to_dir='.', no_input=False, password=None):
         # The first record in the zipfile should be the directory entry for
         # the archive. If it isn't a directory, there's a problem.
         first_filename = zip_file.namelist()[0]
-        if not first_filename.endswith(os.path.sep):
+        if not first_filename.endswith('/'):
             raise InvalidZipRepository(
                 'Zip repository {} does not include '
                 'a top-level directory'.format(zip_uri)
