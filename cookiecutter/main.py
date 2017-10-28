a67111ea669d1b50c10d5dfe8d72673e9e680f99
eruber <eruber@gmail.com>
2017-10-28 00:55:00 -0700

    Call context v1 or v2 processing depending on which version is detected.

--- cookiecutter/main.py
+++ cookiecutter/main.py
@@ -19,6 +19,8 @@ from .replay import dump, load
 from .repository import determine_repo_dir
 from .utils import rmtree
 
+from .context import context_is_version_2, load_context
+
 logger = logging.getLogger(__name__)
 
 
@@ -79,7 +81,12 @@ def cookiecutter(
 
         # prompt the user to manually configure at the command line.
         # except when 'no-input' flag is set
-        context['cookiecutter'] = prompt_for_config(context, no_input)
+        if context_is_version_2(context['cookiecutter']):
+            context['cookiecutter'] = load_context(context[u'cookiecutter'],
+                                                   no_input=no_input,
+                                                   verbose=True)
+        else:
+            context['cookiecutter'] = prompt_for_config(context, no_input)
 
         # include template dir or url in the context dict
         context['cookiecutter']['_template'] = template
