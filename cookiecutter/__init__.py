76d411158fb19a24fba8ed3e6d974f24b88ef04a
eruber <eruber@gmail.com>
2017-10-28 00:51:57 -0700

    Update version to 2.0.0 alpha

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -1,2 +1,10 @@
-"""Main package for Cookiecutter."""
-__version__ = "2.0.0"
+# -*- coding: utf-8 -*-
+
+"""
+cookiecutter
+------------
+
+Main package for Cookiecutter.
+"""
+
+__version__ = '2.0.0-alpha.u+1.6.0'
