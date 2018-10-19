476f34931376545f471cf866a445f12bfeb09a54
skrishnan-aero <skrishnan.aero@gmail.com>
2018-10-20 01:15:06 +0530

    pydocstyle with flake8 on replay.py
    -Added docstrings for all functions.
    -Added descriptions of each step in a function.
    -Improved module docstring to comply with flake8.

--- cookiecutter/replay.py
+++ cookiecutter/replay.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""
-cookiecutter.replay
--------------------
-"""
+"""All functions specific to Replay Project Generation are defined here."""
 
 from __future__ import unicode_literals
 
@@ -15,39 +12,50 @@ from .utils import make_sure_path_exists
 
 
 def get_file_name(replay_dir, template_name):
+    """Generate a JSON file name and return its absolute path."""
     file_name = '{}.json'.format(template_name)
     return os.path.join(replay_dir, file_name)
 
 
 def dump(replay_dir, template_name, context):
+    """Create a replay file to save user input for a template."""
     if not make_sure_path_exists(replay_dir):
+        # Inform the user of the directory being non existent.
         raise IOError('Unable to create replay dir at {}'.format(replay_dir))
 
     if not isinstance(template_name, basestring):
+        # Require the template name only in the form of a string.
         raise TypeError('Template name is required to be of type str')
 
     if not isinstance(context, dict):
+        # Require the context only in the form of a dictionary.
         raise TypeError('Context is required to be of type dict')
 
     if 'cookiecutter' not in context:
+        # Throw an exception if the cookiecutter key is not in the context.
         raise ValueError('Context is required to contain a cookiecutter key')
 
     replay_file = get_file_name(replay_dir, template_name)
 
+    # Write the context to JSON file.
     with open(replay_file, 'w') as outfile:
         json.dump(context, outfile)
 
 
 def load(replay_dir, template_name):
+    """Load the replay file and return its context."""
     if not isinstance(template_name, basestring):
+        # Require the template name only in the form of a string.
         raise TypeError('Template name is required to be of type str')
 
     replay_file = get_file_name(replay_dir, template_name)
 
+    # Load the context from JSON file
     with open(replay_file, 'r') as infile:
         context = json.load(infile)
 
     if 'cookiecutter' not in context:
+        # Throw an exception if the cookiecutter key is not in the context.
         raise ValueError('Context is required to contain a cookiecutter key')
 
     return context
