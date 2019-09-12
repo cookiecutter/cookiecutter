9390057ee4f5b42162da7d20287697a5352ca4aa
Dominic Bevacqua <bev@treatwell.com>
2019-09-12 15:41:50 +0100

    Add tests

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -22,3 +22,20 @@ def test_env_should_raise_for_unknown_extension():
 def test_env_should_come_with_jinja2_time_extension():
     env = StrictEnvironment(keep_trailing_newline=True)
     assert 'jinja2_time.jinja2_time.TimeExtension' in env.extensions
+
+@pytest.fixture(autouse=True)
+def test_env_should_contain_delimiter_overrides(monkeypatch):
+    monkeypatch.setenv('J2_VARIABLE_START_STRING', '<<')
+    monkeypatch.setenv('J2_VARIABLE_END_STRING', '>>')
+    monkeypatch.setenv('J2_BLOCK_START_STRING', '<%')
+    monkeypatch.setenv('J2_BLOCK_END_STRING', '%>')
+    monkeypatch.setenv('J2_COMMENT_START_STRING', '<#')
+    monkeypatch.setenv('J2_COMMENT_END_STRING', '#>')
+
+    env = StrictEnvironment(keep_trailing_newline=True)
+    assert '<<' == env.variable_start_string
+    assert '>>' == env.variable_end_string
+    assert '<%' == env.block_start_string
+    assert '%>' == env.block_end_string
+    assert '<#' == env.comment_start_string
+    assert '#>' == env.comment_end_string
