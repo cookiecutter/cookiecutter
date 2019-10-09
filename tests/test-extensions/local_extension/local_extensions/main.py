48f8fcbd37b6ad0579a35167f662ab80e17260bb
Marco Westerhof <m.westerhof@lukkien.com>
2019-10-09 16:37:58 +0200

    fix for py27

--- tests/test-extensions/local_extension/local_extensions/main.py
+++ tests/test-extensions/local_extension/local_extensions/main.py
@@ -5,5 +5,5 @@ from jinja2.ext import Extension
 
 class FoobarExtension(Extension):
     def __init__(self, environment):
-        super().__init__(environment)
+        super(FoobarExtension, self).__init__(environment)
         environment.filters['foobar'] = lambda v: v * 2
