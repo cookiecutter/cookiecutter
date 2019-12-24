b843268475a326743b6284efa73e4b04cf4fc62b
Kishan Mehta <kishan@scrapinghub.com>
2019-12-24 17:36:23 +0530

    Add docstring for tests/test-extensions/hello_extension/hello_extension.py

--- tests/test-extensions/hello_extension/hello_extension.py
+++ tests/test-extensions/hello_extension/hello_extension.py
@@ -1,19 +1,30 @@
 # -*- coding: utf-8 -*-
 
+"""
+hello_extension.
+
+----------------
+"""
+
 from jinja2 import nodes
 from jinja2.ext import Extension
 
 
 class HelloExtension(Extension):
+    """A hello extension based on jinja2 Extension."""
+
     tags = set(['hello'])
 
     def __init__(self, environment):
+        """__init__."""
         super(HelloExtension, self).__init__(environment)
 
     def _hello(self, name):
+        """Method greets with Hello <name>!."""
         return 'Hello {name}!'.format(name=name)
 
     def parse(self, parser):
+        """parse."""
         lineno = next(parser.stream).lineno
         node = parser.parse_expression()
         call_method = self.call_method('_hello', [node], lineno=lineno)
