e1a4708dbef288d995a5d16d8f892bfa00155be0
Pablo Panero <pablo.panero@cern.ch>
2020-03-30 15:37:56 +0200

    Add slugify template filter

--- cookiecutter/extensions.py
+++ cookiecutter/extensions.py
@@ -11,6 +11,7 @@ except ImportError:
     from random import choice
 
 from jinja2.ext import Extension
+from slugify import slugify as pyslugify
 
 
 class JsonifyExtension(Extension):
@@ -40,3 +41,17 @@ class RandomStringExtension(Extension):
                 corpus = string.ascii_letters
             return "".join(choice(corpus) for _ in range(length))
         environment.globals.update(random_ascii_string=random_ascii_string)
+
+
+class SlugifyExtension(Extension):
+    """Jinja2 Extension to slugify string."""
+
+    def __init__(self, environment):
+        """Jinja2 Extension constructor."""
+        super(SlugifyExtension, self).__init__(environment)
+
+        def slugify(value, **kwargs):
+            """Slugifies the value."""
+            return pyslugify(value, **kwargs)
+
+        environment.filters['slugify'] = slugify
