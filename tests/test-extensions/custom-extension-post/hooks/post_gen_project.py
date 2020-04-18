9ffbe89bd41021c022015179b28227f7bc7f4d4f
Andrey Shpak <insspb@users.noreply.github.com>
2020-04-18 19:30:12 +0300

    Added: docstrings for extensions feature test files (#1373)

--- tests/test-extensions/custom-extension-post/hooks/post_gen_project.py
+++ tests/test-extensions/custom-extension-post/hooks/post_gen_project.py
@@ -3,5 +3,9 @@
 # flake8: noqa
 
 """Sample post-gen hook for testing that custom extensions are available and exposed methods are callable."""
+import sys
 
-print('{% hello cookiecutter.name %}')
+if '{% hello cookiecutter.name %}' == 'Hello Cookiemonster!':
+    sys.exit(0)
+else:
+    sys.exit(1)
