f6193e7f06819ffa5a4514027f0594c50cf10d75
Paul Harrison <paulharrison@fastmail.com>
2020-04-28 20:26:59 +0100

    Lint test.

--- tests/test_prompt.py
+++ tests/test_prompt.py
@@ -209,28 +209,32 @@ class TestPrompt(object):
            are rendered.
         """
         context = {
-            'cookiecutter': OrderedDict([
-                ('foo', 'Hello world'),
-                ('bar', 123),
-                ('rendered_foo', u'{{ cookiecutter.foo|lower }}'),
-                ('rendered_bar', 123),
-                ('_hidden_foo', u'{{ cookiecutter.foo|lower }}'),
-                ('_hidden_bar', 123),
-                ('__rendered_hidden_foo', u'{{ cookiecutter.foo|lower }}'),
-                ('__rendered_hidden_bar', 123),
-            ])
+            'cookiecutter': OrderedDict(
+                [
+                    ('foo', 'Hello world'),
+                    ('bar', 123),
+                    ('rendered_foo', u'{{ cookiecutter.foo|lower }}'),
+                    ('rendered_bar', 123),
+                    ('_hidden_foo', u'{{ cookiecutter.foo|lower }}'),
+                    ('_hidden_bar', 123),
+                    ('__rendered_hidden_foo', u'{{ cookiecutter.foo|lower }}'),
+                    ('__rendered_hidden_bar', 123),
+                ]
+            )
         }
         cookiecutter_dict = prompt.prompt_for_config(context, no_input=True)
-        assert cookiecutter_dict == OrderedDict([
-            ('foo', 'Hello world'),
-            ('bar', '123'),
-            ('rendered_foo', 'hello world'),
-            ('rendered_bar', '123'),
-            ('_hidden_foo', u'{{ cookiecutter.foo|lower }}'),
-            ('_hidden_bar', 123),
-            ('__rendered_hidden_foo', 'hello world'),
-            ('__rendered_hidden_bar', '123'),
-        ])
+        assert cookiecutter_dict == OrderedDict(
+            [
+                ('foo', 'Hello world'),
+                ('bar', '123'),
+                ('rendered_foo', 'hello world'),
+                ('rendered_bar', '123'),
+                ('_hidden_foo', u'{{ cookiecutter.foo|lower }}'),
+                ('_hidden_bar', 123),
+                ('__rendered_hidden_foo', 'hello world'),
+                ('__rendered_hidden_bar', '123'),
+            ]
+        )
 
     def test_should_not_render_private_variables(self):
         """Verify private(underscored) variables not rendered by `prompt_for_config`.
