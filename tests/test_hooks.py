90434ff4ea4477941444f1e83313beb414838535
Nimrod Milo <milo.nimrod@gmail.com>
2020-05-29 19:41:33 +0300

    Changed: Generated projects can use multiple type hooks at same time. (sh + py) (#974)
    * Added support for multiple hooks in a single project
    
    * Added tests for Pull #974
    
    * Fix linting
    
    Co-authored-by: Nimrod Moli <nimrodm@traxretail.com>
    Co-authored-by: Andrey Shpak <ashpak@ashpak.ru>

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -9,7 +9,7 @@ import pytest
 from cookiecutter import hooks, utils, exceptions
 
 
-def make_test_repo(name):
+def make_test_repo(name, multiple_hooks=False):
     """Create test repository for test setup methods."""
     hook_dir = os.path.join(name, 'hooks')
     template = os.path.join(name, 'input{{hooks}}')
@@ -47,6 +47,26 @@ def make_test_repo(name):
         # Set the execute bit
         os.chmod(filename, os.stat(filename).st_mode | stat.S_IXUSR)
 
+    # Adding an additional pre script
+    if multiple_hooks:
+        if sys.platform.startswith('win'):
+            pre = 'pre_gen_project.bat'
+            with open(os.path.join(hook_dir, pre), 'w') as f:
+                f.write("@echo off\n")
+                f.write("\n")
+                f.write("echo post generation hook\n")
+                f.write("echo. >shell_pre.txt\n")
+        else:
+            pre = 'pre_gen_project.sh'
+            filename = os.path.join(hook_dir, pre)
+            with open(filename, 'w') as f:
+                f.write("#!/bin/bash\n")
+                f.write("\n")
+                f.write("echo 'post generation hook';\n")
+                f.write("touch 'shell_pre.txt'\n")
+            # Set the execute bit
+            os.chmod(filename, os.stat(filename).st_mode | stat.S_IXUSR)
+
     return post
 
 
@@ -68,11 +88,11 @@ class TestFindHooks(object):
         with utils.work_in(self.repo_path):
             expected_pre = os.path.abspath('hooks/pre_gen_project.py')
             actual_hook_path = hooks.find_hook('pre_gen_project')
-            assert expected_pre == actual_hook_path
+            assert expected_pre == actual_hook_path[0]
 
             expected_post = os.path.abspath('hooks/{}'.format(self.post_hook))
             actual_hook_path = hooks.find_hook('post_gen_project')
-            assert expected_post == actual_hook_path
+            assert expected_post == actual_hook_path[0]
 
     def test_no_hooks(self):
         """`find_hooks` should return None if the hook could not be found."""
@@ -98,7 +118,7 @@ class TestExternalHooks(object):
 
     def setup_method(self, method):
         """External hooks related tests setup fixture."""
-        self.post_hook = make_test_repo(self.repo_path)
+        self.post_hook = make_test_repo(self.repo_path, multiple_hooks=True)
 
     def teardown_method(self, method):
         """External hooks related tests teardown fixture."""
@@ -108,6 +128,8 @@ class TestExternalHooks(object):
             os.remove('python_pre.txt')
         if os.path.exists('shell_post.txt'):
             os.remove('shell_post.txt')
+        if os.path.exists('shell_pre.txt'):
+            os.remove('shell_pre.txt')
         if os.path.exists('tests/shell_post.txt'):
             os.remove('tests/shell_post.txt')
         if os.path.exists('tests/test-hooks/input{{hooks}}/python_pre.txt'):
@@ -163,6 +185,7 @@ class TestExternalHooks(object):
         with utils.work_in(self.repo_path):
             hooks.run_hook('pre_gen_project', tests_dir, {})
             assert os.path.isfile(os.path.join(tests_dir, 'python_pre.txt'))
+            assert os.path.isfile(os.path.join(tests_dir, 'shell_pre.txt'))
 
             hooks.run_hook('post_gen_project', tests_dir, {})
             assert os.path.isfile(os.path.join(tests_dir, 'shell_post.txt'))
