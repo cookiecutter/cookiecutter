1873b5b1bc07ede10eff84041803b710ecd80dc8
samsy12 <samayan.bhattacharya@gmail.com>
2020-10-02 16:26:41 +0530

    removed stray spaces in __main__.py

--- __main__.py
+++ __main__.py
@@ -2,5 +2,5 @@
 import runpy
 
 
-if __name__ == "__main__":
-    runpy.run_module("cookiecutter", run_name="__main__")
+if __name__=="__main__":
+    runpy.run_module("cookiecutter",run_name="__main__")
