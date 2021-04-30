0fcc09bf9ee677743c72a18dd1b2fd77d14ee124
Sebastian Straub <sebastian.straub@alexanderthamm.com>
2021-04-30 13:35:09 +0200

    schema version, validation, detection
    - changed the schema definition so "version" defines the version of the
    cookiecutter schema and is a strict requirement
    - cookiecutter_version is optional and defines the min required
    cookiecutter version
    - template_version describes the version number of the cookiecutter
    template
    - added descriptions for all schema fields
    - validate() function improved, added the detect() function
    - added documentation for these
    - more unit tests

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,15 +1,58 @@
 import json
 
-from cookiecutter.schema import validate
+import pytest
+from jsonschema import ValidationError
+
+from cookiecutter.schema import detect, validate
 
 
-def test_validate_1_0_0():
-    with open('tests/test-context/cookiecutter-1.0.0.json') as fp:
-        d = json.load(fp)
-        validate(d, version='1.0.0')
+def test_validate_1_0():
+    d = get_sample_cookiecutter('1.0')
+    validate(d, version='1.0')
 
 
-def test_validate_2_0_0():
-    with open('tests/test-context/cookiecutter-2.0.0.json') as fp:
-        d = json.load(fp)
-        validate(d, version='2.0.0')
+def test_validate_fail_1_0():
+    d = get_sample_cookiecutter('1.0')
+    d['my_dict'] = {'dicts': 'are not supported'}
+    with pytest.raises(ValidationError):
+        validate(d, version='1.0')
+
+
+def test_validate_2_0():
+    d = get_sample_cookiecutter('2.0')
+    validate(d, version='2.0')
+
+
+def test_validate_fail_2_0():
+    d = get_sample_cookiecutter('2.0')
+    d['authors'] = "authors must be an array"
+    with pytest.raises(ValidationError):
+        validate(d, version='2.0')
+
+
+def test_validate_fail_unsupported():
+    d = get_sample_cookiecutter('2.0')
+    d['version'] = "2.42"
+    with pytest.raises(ValueError):
+        validate(d)
+
+
+def test_detect_1_0():
+    d = get_sample_cookiecutter('1.0')
+    assert detect(d) == '1.0'
+
+
+def test_detect_2_0():
+    d = get_sample_cookiecutter('2.0')
+    assert detect(d) == '2.0'
+
+
+def test_detect_invalid():
+    d = get_sample_cookiecutter('2.0')
+    d['version'] = "2.42"
+    assert detect(d) is None
+
+
+def get_sample_cookiecutter(version='2.0.0'):
+    with open(f'test-context/cookiecutter-{version}.json') as fp:
+        return json.load(fp)
