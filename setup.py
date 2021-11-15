509a9fadbb482b0f4eb1b4947e87833f39cac4ff
Christo <christo.delange@roadsync.com>
2021-11-15 09:41:45 -0500

    wheel

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ import setuptools
 # https://github.com/jazzband/pip-tools/issues/1278
 setuptools.setup(
     use_scm_version={"local_scheme": "no-local-version"},
-    setup_requires=["setuptools_scm[toml]>=3.5.0"],
+    setup_requires=["setuptools_scm[toml]>=3.5.0", "wheel"],
 )
