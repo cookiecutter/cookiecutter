d233e33e288e3f573c0f1f1206137c1344c562ef
Andrey Shpak <ashpak@ashpak.ru>
2022-06-08 14:46:03 +0300

    Tests: Use pathlib for files read/write (test_hooks.py)

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,9 +1,10 @@
 """Tests for `cookiecutter.hooks` module."""
-import os
 import errno
+import os
 import stat
 import sys
 import textwrap
+from pathlib import Path
 
 import pytest
 
@@ -18,10 +19,10 @@ def make_test_repo(name, multiple_hooks=False):
     os.mkdir(hook_dir)
     os.mkdir(template)
 
-    with open(os.path.join(template, 'README.rst'), 'w') as f:
+    with Path(template, 'README.rst').open('w') as f:
         f.write("foo\n===\n\nbar\n")
 
-    with open(os.path.join(hook_dir, 'pre_gen_project.py'), 'w') as f:
+    with Path(hook_dir, 'pre_gen_project.py').open('w') as f:
         f.write("#!/usr/bin/env python\n")
         f.write("# -*- coding: utf-8 -*-\n")
         f.write("from __future__ import print_function\n")
@@ -32,7 +33,7 @@ def make_test_repo(name, multiple_hooks=False):
 
     if sys.platform.startswith('win'):
         post = 'post_gen_project.bat'
-        with open(os.path.join(hook_dir, post), 'w') as f:
+        with Path(hook_dir, post).open('w') as f:
             f.write("@echo off\n")
             f.write("\n")
             f.write("echo post generation hook\n")
@@ -40,7 +41,7 @@ def make_test_repo(name, multiple_hooks=False):
     else:
         post = 'post_gen_project.sh'
         filename = os.path.join(hook_dir, post)
-        with open(filename, 'w') as f:
+        with Path(filename).open('w') as f:
             f.write("#!/bin/bash\n")
             f.write("\n")
             f.write("echo 'post generation hook';\n")
@@ -52,7 +53,7 @@ def make_test_repo(name, multiple_hooks=False):
     if multiple_hooks:
         if sys.platform.startswith('win'):
             pre = 'pre_gen_project.bat'
-            with open(os.path.join(hook_dir, pre), 'w') as f:
+            with Path(hook_dir, pre).open('w') as f:
                 f.write("@echo off\n")
                 f.write("\n")
                 f.write("echo post generation hook\n")
@@ -60,7 +61,7 @@ def make_test_repo(name, multiple_hooks=False):
         else:
             pre = 'pre_gen_project.sh'
             filename = os.path.join(hook_dir, pre)
-            with open(filename, 'w') as f:
+            with Path(filename).open('w') as f:
                 f.write("#!/bin/bash\n")
                 f.write("\n")
                 f.write("echo 'post generation hook';\n")
@@ -182,13 +183,13 @@ class TestExternalHooks:
 
         if sys.platform.startswith('win'):
             post = 'post_gen_project.bat'
-            with open(os.path.join(self.hooks_path, post), 'w') as f:
+            with Path(self.hooks_path, post).open('w') as f:
                 f.write("@echo off\n")
                 f.write("\n")
                 f.write("echo post generation hook\n")
                 f.write("echo. >{{cookiecutter.file}}\n")
         else:
-            with open(hook_path, 'w') as fh:
+            with Path(hook_path).open('w') as fh:
                 fh.write("#!/bin/bash\n")
                 fh.write("\n")
                 fh.write("echo 'post generation hook';\n")
@@ -221,7 +222,7 @@ class TestExternalHooks:
         hook_path = os.path.join(self.hooks_path, 'pre_gen_project.py')
         tests_dir = os.path.join(self.repo_path, 'input{{hooks}}')
 
-        with open(hook_path, 'w') as f:
+        with Path(hook_path).open('w') as f:
             f.write("#!/usr/bin/env python\n")
             f.write("import sys; sys.exit(1)\n")
 
