8413ed9a71940d7f3399da18b23c77c1cc3fb5dc
Andrey Shpak <ashpak@ashpak.ru>
2022-06-08 16:28:49 +0300

    Tests: Use pathlib read_text/write_text with encoding (test_generate_file.py)

--- tests/test_generate_file.py
+++ tests/test_generate_file.py
@@ -46,9 +46,8 @@ def test_generate_file(env):
         env=env,
     )
     assert os.path.isfile('tests/files/cheese.txt')
-    with Path('tests/files/cheese.txt').open() as f:
-        generated_text = f.read()
-        assert generated_text == 'Testing cheese'
+    generated_text = Path('tests/files/cheese.txt').read_text()
+    assert generated_text == 'Testing cheese'
 
 
 def test_generate_file_jsonify_filter(env):
@@ -59,9 +58,8 @@ def test_generate_file_jsonify_filter(env):
         project_dir=".", infile=infile, context={'cookiecutter': data}, env=env
     )
     assert os.path.isfile('tests/files/cheese.txt')
-    with Path('tests/files/cheese.txt').open() as f:
-        generated_text = f.read()
-        assert json.loads(generated_text) == data
+    generated_text = Path('tests/files/cheese.txt').read_text()
+    assert json.loads(generated_text) == data
 
 
 @pytest.mark.parametrize("length", (10, 40))
@@ -73,9 +71,8 @@ def test_generate_file_random_ascii_string(env, length, punctuation):
     context = {"cookiecutter": data, "length": length, "punctuation": punctuation}
     generate.generate_file(project_dir=".", infile=infile, context=context, env=env)
     assert os.path.isfile('tests/files/cheese.txt')
-    with Path('tests/files/cheese.txt').open() as f:
-        generated_text = f.read()
-        assert len(generated_text) == length
+    generated_text = Path('tests/files/cheese.txt').read_text()
+    assert len(generated_text) == length
 
 
 def test_generate_file_with_true_condition(env):
@@ -93,9 +90,8 @@ def test_generate_file_with_true_condition(env):
         env=env,
     )
     assert os.path.isfile('tests/files/cheese.txt')
-    with Path('tests/files/cheese.txt').open() as f:
-        generated_text = f.read()
-        assert generated_text == 'Testing that generate_file was y'
+    generated_text = Path('tests/files/cheese.txt').read_text()
+    assert generated_text == 'Testing that generate_file was y'
 
 
 def test_generate_file_with_false_condition(env):
@@ -149,7 +145,7 @@ def test_generate_file_does_not_translate_lf_newlines_to_crlf(env, tmp_path):
 
     # this generated file should have a LF line ending
     gf = 'tests/files/cheese_lf_newlines.txt'
-    with Path(gf).open(newline='') as f:
+    with Path(gf).open(encoding='utf-8', newline='') as f:
         simple_text = f.readline()
     assert simple_text == 'newline is LF\n'
     assert f.newlines == '\n'
@@ -167,7 +163,7 @@ def test_generate_file_does_not_translate_crlf_newlines_to_lf(env):
 
     # this generated file should have a CRLF line ending
     gf = 'tests/files/cheese_crlf_newlines.txt'
-    with Path(gf).open(newline='') as f:
+    with Path(gf).open(encoding='utf-8', newline='') as f:
         simple_text = f.readline()
     assert simple_text == 'newline is CRLF\r\n'
     assert f.newlines == '\r\n'
