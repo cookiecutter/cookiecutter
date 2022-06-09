97c7fcc7ada0f8ac0d34aa98e4a8347fbd676a81
Andrey Shpak <ashpak@ashpak.ru>
2022-06-09 20:01:51 +0300

    Update strings in utils.py with force black

--- cookiecutter/utils.py
+++ cookiecutter/utils.py
@@ -37,11 +37,11 @@ def make_sure_path_exists(path: "os.PathLike[str]") -> None:
 
     :param path: A directory tree path for creation.
     """
-    logger.debug('Making sure path exists (creates tree if not exist): %s', path)
+    logger.debug("Making sure path exists (creates tree if not exist): %s", path)
     try:
         Path(path).mkdir(parents=True, exist_ok=True)
     except OSError as error:
-        raise OSError(f'Unable to create directory at {path}') from error
+        raise OSError(f"Unable to create directory at {path}") from error
 
 
 @contextlib.contextmanager
@@ -87,7 +87,7 @@ def prompt_and_delete(path, no_input=False):
             f"You've downloaded {path} before. Is it okay to delete and re-download it?"
         )
 
-        ok_to_delete = read_user_yes_no(question, 'yes')
+        ok_to_delete = read_user_yes_no(question, "yes")
 
     if ok_to_delete:
         if os.path.isdir(path):
@@ -97,7 +97,7 @@ def prompt_and_delete(path, no_input=False):
         return True
     else:
         ok_to_reuse = read_user_yes_no(
-            "Do you want to re-use the existing version?", 'yes'
+            "Do you want to re-use the existing version?", "yes"
         )
 
         if ok_to_reuse:
