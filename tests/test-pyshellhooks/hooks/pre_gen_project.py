306f687e6750776647204b5008d89151d6f873ee
Andrey Shpak <ashpak@ashpak.ru>
2022-06-09 20:17:03 +0300

    Update strings in tests/hooks with force black

--- tests/test-pyshellhooks/hooks/pre_gen_project.py
+++ tests/test-pyshellhooks/hooks/pre_gen_project.py
@@ -1,6 +1,6 @@
 """Simple pre-gen hook for testing project folder and custom file creation."""
 
 
-print('pre generation hook')
-f = open('python_pre.txt', 'w')
+print("pre generation hook")
+f = open("python_pre.txt", "w")
 f.close()
