51ed78b35303e5577ddedb5dbae03373f7ac23b6
Tim Gates <tim.gates@iress.com>
2022-07-03 19:06:41 +1000

    docs: fix simple typo, shat -> that
    There is a small typo in docs/conf.py.
    
    Should read `that` rather than `shat`.

--- docs/conf.py
+++ docs/conf.py
@@ -327,7 +327,7 @@ epub_copyright = '2013-2022, Audrey Roy and Cookiecutter community'
 # The format is a list of tuples containing the path and title.
 # epub_pre_files = []
 
-# HTML files shat should be inserted after the pages created by sphinx.
+# HTML files that should be inserted after the pages created by sphinx.
 # The format is a list of tuples containing the path and title.
 # epub_post_files = []
 
