4120c37ce7665a902da40f9613eebac4042c3e56
Aleksey Kuznetsov <alexei.kuznetsov@corp.mail.ru>
2022-07-28 17:03:04 +0300

    add implementation

--- cookiecutter/main.py
+++ cookiecutter/main.py
@@ -87,32 +87,47 @@ def cookiecutter(
     if replay:
         with import_patch:
             if isinstance(replay, bool):
-                context = load(config_dict['replay_dir'], template_name)
+                context_from_replayfile = load(config_dict['replay_dir'], template_name)
             else:
                 path, template_name = os.path.split(os.path.splitext(replay)[0])
-                context = load(path, template_name)
-    else:
-        context_file = os.path.join(repo_dir, 'cookiecutter.json')
-        logger.debug('context_file is %s', context_file)
+                context_from_replayfile = load(path, template_name)
+                
+    context_file = os.path.join(repo_dir, 'cookiecutter.json')
+    logger.debug('context_file is %s', context_file)
 
+    if replay:
+        context = generate_context(
+            context_file=context_file,
+            default_context=config_dict['default_context'],
+            extra_context=None,
+        )
+        logger.debug('replayfile context: %s', context_from_replayfile)
+        items_for_prompting = { k:v for k,v in context['cookiecutter'].items() if k not in context_from_replayfile['cookiecutter'].keys() }
+        context_for_prompting = {}
+        context_for_prompting['cookiecutter'] = items_for_prompting
+        logger.debug('prompting context: %s', context_for_prompting)
+    else:
         context = generate_context(
             context_file=context_file,
             default_context=config_dict['default_context'],
             extra_context=extra_context,
         )
+        context_for_prompting = context
 
-        # prompt the user to manually configure at the command line.
-        # except when 'no-input' flag is set
-        with import_patch:
-            context['cookiecutter'] = prompt_for_config(context, no_input)
+    # prompt the user to manually configure at the command line.
+    # except when 'no-input' flag is set
+    
+    with import_patch:
+        context['cookiecutter'].update(prompt_for_config(context_for_prompting, no_input))
+    logger.debug('contex is %s', context)
 
-        # include template dir or url in the context dict
-        context['cookiecutter']['_template'] = template
+    # include template dir or url in the context dict
+    context['cookiecutter']['_template'] = template
 
-        # include output+dir in the context dict
-        context['cookiecutter']['_output_dir'] = os.path.abspath(output_dir)
+    # include output+dir in the context dict
+    context['cookiecutter']['_output_dir'] = os.path.abspath(output_dir)
 
-        dump(config_dict['replay_dir'], template_name, context)
+    dump(config_dict['replay_dir'], template_name, context)
 
     # Create project from local context and project template.
     with import_patch:
