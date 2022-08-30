dced06cacec7b81afd9086ed9602666a91602097
Dario Curreri <dariocurr@gmail.com>
2022-08-30 16:23:32 +0200

    fix bug windows

--- tests/test_cookiecutter_nested_templates.py
+++ tests/test_cookiecutter_nested_templates.py
@@ -1,10 +1,14 @@
 """Test cookiecutter invocation with nested configuration structure."""
+import os
+
 from cookiecutter import main
 
 
 def test_cookiecutter_nested_templates(mocker):
     """Verify cookiecutter nested configuration files mechanism."""
     mock_generate_files = mocker.patch("cookiecutter.main.generate_files")
-    main_dir = "tests/fake-nested-templates"
+    main_dir = os.path.join("tests", "fake-nested-templates")
     main.cookiecutter(main_dir, no_input=True)
-    assert mock_generate_files.call_args[1]["repo_dir"] == f"{main_dir}/fake-project"
+    assert mock_generate_files.call_args[1]["repo_dir"] == os.path.join(
+        main_dir, "fake-project"
+    )
