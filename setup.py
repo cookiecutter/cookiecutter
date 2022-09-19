88135c1634ccd425831e9e6701be9376102da4a2
Sorin Sbarnea <sorin.sbarnea@gmail.com>
2022-09-19 10:50:12 +0100

    Enable py311 support
    - enable py311 pipelines
    - expose py311 support in metadata

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Programming Language :: Python",
