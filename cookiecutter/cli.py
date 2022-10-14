c02387a75acf43ed67d4810b528488713f79cd97
sayampradhan <112542130+sayampradhan@users.noreply.github.com>
2022-10-14 23:23:45 +0530

    Format Code

--- cookiecutter/cli.py
+++ cookiecutter/cli.py
@@ -33,7 +33,7 @@ def version_msg():
 def validate_extra_context(ctx, param, value):
     """Validate extra context."""
     for string in value:
-        if '=' not in string:
+        if "=" not in string:
             raise click.BadParameter(
                 f"EXTRA_CONTEXT should contain items of the form key=value; "
                 f"'{string}' doesn't match that form"
@@ -41,13 +41,13 @@ def validate_extra_context(ctx, param, value):
 
     # Convert tuple -- e.g.: ('program_name=foobar', 'startsecs=66')
     # to dict -- e.g.: {'program_name': 'foobar', 'startsecs': '66'}
-    return collections.OrderedDict(s.split('=', 1) for s in value) or None
+    return collections.OrderedDict(s.split("=", 1) for s in value) or None
 
 
 def list_installed_templates(default_config, passed_config_file):
     """List installed (locally cloned) templates. Use cookiecutter --list-installed."""
     config = get_user_config(passed_config_file, default_config)
-    cookiecutter_folder = config.get('cookiecutters_dir')
+    cookiecutter_folder = config.get("cookiecutters_dir")
     if not os.path.exists(cookiecutter_folder):
         click.echo(
             f"Error: Cannot list installed templates. "
@@ -59,97 +59,97 @@ def list_installed_templates(default_config, passed_config_file):
         folder
         for folder in os.listdir(cookiecutter_folder)
         if os.path.exists(
-            os.path.join(cookiecutter_folder, folder, 'cookiecutter.json')
+            os.path.join(cookiecutter_folder, folder, "cookiecutter.json")
         )
     ]
-    click.echo(f'{len(template_names)} installed templates: ')
+    click.echo(f"{len(template_names)} installed templates: ")
     for name in template_names:
-        click.echo(f' * {name}')
+        click.echo(f" * {name}")
 
 
-@click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.version_option(__version__, '-V', '--version', message=version_msg())
-@click.argument('template', required=False)
-@click.argument('extra_context', nargs=-1, callback=validate_extra_context)
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
+@click.version_option(__version__, "-V", "--version", message=version_msg())
+@click.argument("template", required=False)
+@click.argument("extra_context", nargs=-1, callback=validate_extra_context)
 @click.option(
-    '--no-input',
+    "--no-input",
     is_flag=True,
-    help='Do not prompt for parameters and only use cookiecutter.json file content. '
-    'Defaults to deleting any cached resources and redownloading them. '
-    'Cannot be combined with the --replay flag.',
+    help="Do not prompt for parameters and only use cookiecutter.json file content. "
+    "Defaults to deleting any cached resources and redownloading them. "
+    "Cannot be combined with the --replay flag.",
 )
 @click.option(
-    '-c',
-    '--checkout',
-    help='branch, tag or commit to checkout after git clone',
+    "-c",
+    "--checkout",
+    help="branch, tag or commit to checkout after git clone",
 )
 @click.option(
-    '--directory',
-    help='Directory within repo that holds cookiecutter.json file '
-    'for advanced repositories with multi templates in it',
+    "--directory",
+    help="Directory within repo that holds cookiecutter.json file "
+    "for advanced repositories with multi templates in it",
 )
 @click.option(
-    '-v', '--verbose', is_flag=True, help='Print debug information', default=False
+    "-v", "--verbose", is_flag=True, help="Print debug information", default=False
 )
 @click.option(
-    '--replay',
+    "--replay",
     is_flag=True,
-    help='Do not prompt for parameters and only use information entered previously. '
-    'Cannot be combined with the --no-input flag or with extra configuration passed.',
+    help="Do not prompt for parameters and only use information entered previously. "
+    "Cannot be combined with the --no-input flag or with extra configuration passed.",
 )
 @click.option(
-    '--replay-file',
+    "--replay-file",
     type=click.Path(),
     default=None,
-    help='Use this file for replay instead of the default.',
+    help="Use this file for replay instead of the default.",
 )
 @click.option(
-    '-f',
-    '--overwrite-if-exists',
+    "-f",
+    "--overwrite-if-exists",
     is_flag=True,
-    help='Overwrite the contents of the output directory if it already exists',
+    help="Overwrite the contents of the output directory if it already exists",
 )
 @click.option(
-    '-s',
-    '--skip-if-file-exists',
+    "-s",
+    "--skip-if-file-exists",
     is_flag=True,
-    help='Skip the files in the corresponding directories if they already exist',
+    help="Skip the files in the corresponding directories if they already exist",
     default=False,
 )
 @click.option(
-    '-o',
-    '--output-dir',
-    default='.',
+    "-o",
+    "--output-dir",
+    default=".",
     type=click.Path(),
-    help='Where to output the generated project dir into',
+    help="Where to output the generated project dir into",
 )
 @click.option(
-    '--config-file', type=click.Path(), default=None, help='User configuration file'
+    "--config-file", type=click.Path(), default=None, help="User configuration file"
 )
 @click.option(
-    '--default-config',
+    "--default-config",
     is_flag=True,
-    help='Do not load a config file. Use the defaults instead',
+    help="Do not load a config file. Use the defaults instead",
 )
 @click.option(
-    '--debug-file',
+    "--debug-file",
     type=click.Path(),
     default=None,
-    help='File to be used as a stream for DEBUG logging',
+    help="File to be used as a stream for DEBUG logging",
 )
 @click.option(
-    '--accept-hooks',
-    type=click.Choice(['yes', 'ask', 'no']),
-    default='yes',
-    help='Accept pre/post hooks',
+    "--accept-hooks",
+    type=click.Choice(["yes", "ask", "no"]),
+    default="yes",
+    help="Accept pre/post hooks",
 )
 @click.option(
-    '-l', '--list-installed', is_flag=True, help='List currently installed templates.'
+    "-l", "--list-installed", is_flag=True, help="List currently installed templates."
 )
 @click.option(
-    '--keep-project-on-failure',
+    "--keep-project-on-failure",
     is_flag=True,
-    help='Do not delete project folder on failure',
+    help="Do not delete project folder on failure",
 )
 def main(
     template,
@@ -182,11 +182,11 @@ def main(
         sys.exit(0)
 
     # Raising usage, after all commands that should work without args.
-    if not template or template.lower() == 'help':
+    if not template or template.lower() == "help":
         click.echo(click.get_current_context().get_help())
         sys.exit(0)
 
-    configure_logger(stream_level='DEBUG' if verbose else 'INFO', debug_file=debug_file)
+    configure_logger(stream_level="DEBUG" if verbose else "INFO", debug_file=debug_file)
 
     # If needed, prompt the user to ask whether or not they want to execute
     # the pre/post hooks.
@@ -209,7 +209,7 @@ def main(
             output_dir=output_dir,
             config_file=config_file,
             default_config=default_config,
-            password=os.environ.get('COOKIECUTTER_REPO_PASSWORD'),
+            password=os.environ.get("COOKIECUTTER_REPO_PASSWORD"),
             directory=directory,
             skip_if_file_exists=skip_if_file_exists,
             accept_hooks=_accept_hooks,
@@ -228,11 +228,11 @@ def main(
         click.echo(e)
         sys.exit(1)
     except UndefinedVariableInTemplate as undefined_err:
-        click.echo(f'{undefined_err.message}')
-        click.echo(f'Error message: {undefined_err.error.message}')
+        click.echo(f"{undefined_err.message}")
+        click.echo(f"Error message: {undefined_err.error.message}")
 
         context_str = json.dumps(undefined_err.context, indent=4, sort_keys=True)
-        click.echo(f'Context: {context_str}')
+        click.echo(f"Context: {context_str}")
         sys.exit(1)
 
 
