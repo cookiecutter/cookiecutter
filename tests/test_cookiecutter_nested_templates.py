ce24aa019c4cdacece99041b06048204f6986277
liortct <liortct@gmail.com>
2023-01-11 14:24:01 +0100

    feat: change os.path.join to path.join

--- tests/test_cookiecutter_nested_templates.py
+++ tests/test_cookiecutter_nested_templates.py
@@ -1,5 +1,5 @@
 """Test cookiecutter invocation with nested configuration structure."""
-import os
+from os import path
 
 from cookieninja import main
 
@@ -7,8 +7,8 @@ from cookieninja import main
 def test_cookiecutter_nested_templates(mocker):
     """Verify cookiecutter nested configuration files mechanism."""
     mock_generate_files = mocker.patch("cookieninja.main.generate_files")
-    main_dir = os.path.join("tests", "fake-nested-templates")
+    main_dir = path.join("tests", "fake-nested-templates")
     main.cookiecutter(main_dir, no_input=True)
-    assert mock_generate_files.call_args[1]["repo_dir"] == os.path.join(
+    assert mock_generate_files.call_args[1]["repo_dir"] == path.join(
         main_dir, "fake-project"
     )
