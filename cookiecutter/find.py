2533a5a40bdd76cd6c6208222464bd219860823f
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-05-03 02:20:11 +0200

    Use enviorments in find function

--- cookiecutter/find.py
+++ cookiecutter/find.py
@@ -2,22 +2,28 @@
 import logging
 import os
 from pathlib import Path
+from jinja2 import Environment
 
 from cookiecutter.exceptions import NonTemplatedInputDirException
 
 logger = logging.getLogger(__name__)
 
 
-def find_template(repo_dir: "os.PathLike[str]") -> Path:
+def find_template(repo_dir: "os.PathLike[str]", env=Environment()) -> Path:
     """Determine which child directory of ``repo_dir`` is the project template.
 
     :param repo_dir: Local directory of newly cloned repo.
+    :param jinja_env: Jinja2 environment.
     :return: Relative path to project template.
     """
     logger.debug('Searching %s for the project template.', repo_dir)
 
     for str_path in os.listdir(repo_dir):
-        if 'cookiecutter' in str_path and '{{' in str_path and '}}' in str_path:
+        if (
+            'cookiecutter' in str_path
+            and env.variable_start_string in str_path
+            and env.variable_end_string in str_path
+        ):
             project_template = Path(repo_dir, str_path)
             break
     else:
