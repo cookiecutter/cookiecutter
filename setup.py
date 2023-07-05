1c5adc06eb47ee7725d362f21ef13fcfb69e041f
Érico Andrei <ericof@gmail.com>
2023-07-05 18:40:31 -0300

    Prepare release 2.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """cookiecutter distutils configuration."""
 from setuptools import setup
 
-version = "2.1.2.dev0"
+version = "2.2.0.dev0"
 
 with open('README.md', encoding='utf-8') as readme_file:
     readme = readme_file.read()
