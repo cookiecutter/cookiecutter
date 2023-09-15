cc5645a82df65fffe6d89b454910cb80f590df26
Russell Martin <russell@rjm.li>
2023-09-15 12:56:20 -0400

    Use UTF-8 for file reading/writing - Read `VERSION.txt` using UTF-8 - Read and write replay JSON using UTF-8

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -6,7 +6,7 @@ def _get_version() -> str:
     """Read VERSION.txt and return its contents."""
     path = Path(__file__).parent.resolve()
     version_file = path / "VERSION.txt"
-    return version_file.read_text().strip()
+    return version_file.read_text(encoding="utf-8").strip()
 
 
 __version__ = _get_version()
