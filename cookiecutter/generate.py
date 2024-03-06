f00642b4d68a8ac1b4864fcfcd26af0d664da7bb
Tyler Milner <tylermilner@gmail.com>
2024-03-05 21:48:18 -0600

    fix: convert strings to booleans for boolean config variables (#1973)
    Fixes a bug where the override value provided via command line wasn't being properly converted to its boolean counterpart.
    
    Since override values from the command line are interpreted as strings, we need to convert the string value to a boolean if the config variable is a boolean to begin with.

--- cookiecutter/generate.py
+++ cookiecutter/generate.py
@@ -12,6 +12,7 @@ from pathlib import Path
 from binaryornot.check import is_binary
 from jinja2 import Environment, FileSystemLoader
 from jinja2.exceptions import TemplateSyntaxError, UndefinedError
+from rich.prompt import InvalidResponse
 
 from cookiecutter.exceptions import (
     ContextDecodingException,
@@ -20,6 +21,7 @@ from cookiecutter.exceptions import (
 )
 from cookiecutter.find import find_template
 from cookiecutter.hooks import run_hook_from_repo_dir
+from cookiecutter.prompt import YesNoPrompt
 from cookiecutter.utils import (
     create_env_with_context,
     make_sure_path_exists,
@@ -96,6 +98,16 @@ def apply_overwrites_to_context(
                 context_value, overwrite, in_dictionary_variable=True
             )
             context[variable] = context_value
+        elif isinstance(context_value, bool) and isinstance(overwrite, str):
+            # We are dealing with a boolean variable
+            # Convert overwrite to its boolean counterpart
+            try:
+                context[variable] = YesNoPrompt().process_response(overwrite)
+            except InvalidResponse:
+                raise ValueError(
+                    f"{overwrite} provided for variable "
+                    f"{variable} could not be converted to a boolean."
+                )
         else:
             # Simply overwrite the value for this variable
             context[variable] = overwrite
