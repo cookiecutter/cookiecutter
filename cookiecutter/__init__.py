b2c859730e22928dc20ce53aed51e97bf0537817
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2024-05-24 19:17:10 +0800

    refactor: make _get_version work in python binary
    see https://github.com/GreenDeploy-io/cookiecutter-for-binary/wiki/Why%3F for details
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- cookiecutter/__init__.py
+++ cookiecutter/__init__.py
@@ -1,13 +1,16 @@
 """Main package for Cookiecutter."""
 
-from pathlib import Path
+import importlib.resources
 
 
 def _get_version() -> str:
     """Read VERSION.txt and return its contents."""
-    path = Path(__file__).parent.resolve()
-    version_file = path / "VERSION.txt"
-    return version_file.read_text(encoding="utf-8").strip()
+
+    return (
+        importlib.resources.read_text(__package__, 'VERSION.txt').strip()
+        if importlib.resources.is_resource(__package__, 'VERSION.txt')
+        else 'unknown'
+    )
 
 
 __version__ = _get_version()
