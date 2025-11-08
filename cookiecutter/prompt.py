0dfb35603bfd8669d38b0a4957ff213c17e401d9
Jens W. Klein <jk@kleinundpartner.at>
2025-11-08 11:24:59 +0100

    Update cookiecutter/prompt.py

--- cookiecutter/prompt.py
+++ cookiecutter/prompt.py
@@ -275,8 +275,8 @@ def prompt_choice_for_config(
     """
     rendered_options = [render_variable(env, raw, cookiecutter_dict) for raw in options]
     if no_input:
-        if len(rendered_options) == 0:
-            return ""
+        if not rendered_options:
+            raise ValueError("The list of choices is empty")
         return rendered_options[0]
     return read_user_choice(key, rendered_options, prompts, prefix)
 
